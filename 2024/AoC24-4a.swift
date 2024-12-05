a1ee0beb29038b4ff99a0e1a391076b197c254b3
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2024-12-05 11:00:32 -0500

    Update AoC24-4a.swift
    Function calls -> for loop

--- 2024/AoC24-4a.swift
+++ 2024/AoC24-4a.swift
@@ -7,20 +7,21 @@ extension Collection {
 }
 
 func getValue(of input: [[Character]], at ij: [Int]) -> String {
-  if let a = input[safe: ij[0]] {
-    if let b = x[safe: ij[1]] {
-      return String(b)
+  if let x = input[safe: ij[0]] {
+    if let y = x[safe: ij[1]] {
+      return String(y)
     }
   }
   return ""
 }
 
 let input = (try? String(contentsOf: URL(fileURLWithPath: "sample.txt")))!
-let tempInput = input.components(separatedBy: "\n")
+
+let tempIn = input.components(separatedBy: "\n")
 
 var wordSearch = [[Character]]()
 
-for s in tempInput {
+for s in tempIn {
   wordSearch.append(Array(s))
 }
 
@@ -30,14 +31,19 @@ for i in 0..<wordSearch.count {
     if wordSearch[i][j] == "X" {
       var tempStrings: [String] = ["", "", "", "", "", "", "", ""]
       for k in 1...3 {
-        tempStrings[0] += getValue(of: wordSearch, at: [i, j+k])  
-        tempStrings[1] += getValue(of: wordSearch, at: [i+k, j+k])
-        tempStrings[2] += getValue(of: wordSearch, at: [i+k, j])
-        tempStrings[3] += getValue(of: wordSearch, at: [i+k, j-k])
-        tempStrings[4] += getValue(of: wordSearch, at: [i, j-k])
-        tempStrings[5] += getValue(of: wordSearch, at: [i-k, j-k])
-        tempStrings[6] += getValue(of: wordSearch, at: [i-k, j])
-        tempStrings[7] += getValue(of: wordSearch, at: [i-k, j+k])
+        let locations: [[Int]] = [
+          [i, j+k],
+          [i+k, j+k],
+          [i+k, j],
+          [i+k, j-k],
+          [i, j-k],
+          [i-k, j-k],
+          [i-k, j],
+          [i-k, j+k]
+        ]
+        for m in 0..<8 {
+          tempStrings[m] += getValue(of: wordSearch, at: locations[m])
+        }
         for s in tempStrings {
           if s == "MAS" {
             count += 1
