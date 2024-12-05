c490c60d0077c4d889aca88f29004ae89cb561d7
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2024-12-05 11:42:39 -0500

    Update AoC24-4a.swift

--- 2024/AoC24-4a.swift
+++ 2024/AoC24-4a.swift
@@ -23,7 +23,7 @@ for s in input.components(separatedBy: "\n") {
   wordSearch.append(Array(s))
 }
 
-var total1 = 0
+var total1 = 0, total2 = 0
 for i in 0..<wordSearch.count {
   for j in 0..<wordSearch[i].count {
     if wordSearch[i][j] == "X" {
@@ -42,14 +42,34 @@ for i in 0..<wordSearch.count {
         for m in 0..<8 {
           tempStrings[m] += getValue(of: wordSearch, at: locations[m])
         }
-        for s in tempStrings {
-          if s == "MAS" {
-            total1 += 1
-          }
+        for s in tempStrings where s == "MAS" {
+          total1 += 1
         }
+      }
+    } else if wordSearch[i][j] == "A" {
+      let locations: [[Int]] = [
+        [i-1, j-1],
+        [i+1, j-1],
+        [i+1, j+1],
+        [i-1, j+1]
+      ]
+      var mCount = 0, sCount = 0
+      var temp = [String]()
+      for k in locations {
+        let tempChar = getValue(of: wordSearch, at: k)
+        temp.append(tempChar)
+        if tempChar == "M" {
+          mCount += 1
+        } else if tempChar == "S" {
+          sCount += 1
+        }
+      }
+      if mCount == 2 && sCount == 2 && temp[0] != temp[2] && temp[1] != temp[3] {
+        total2 += 1
       }
     }
   }
 }
 
 print("Part 1 answer: \(total1)")
+print("Part 2 answer: \(total2)")
