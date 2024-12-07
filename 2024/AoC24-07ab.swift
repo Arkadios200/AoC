7ca9d3c575d032e3c81a159b8074af48bd0ae28c
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2024-12-07 11:41:57 -0500

    Create AoC24-07ab.swift

---
+++ 2024/AoC24-07ab.swift
@@ -0,0 +1,74 @@
+import Foundation
+
+let input = (try? String(contentsOf: URL(fileURLWithPath: "input.txt")))!.components(separatedBy: "\n")
+var totals = [Int]()
+var lists = [[Int]]()
+for line in input {
+  var tempNums: [String.SubSequence] = line.split(separator: " ")
+  tempNums[0] = tempNums[0][tempNums[0].startIndex...tempNums[0].index(tempNums[0].endIndex, offsetBy:-2)]
+  totals.append(Int(tempNums[0])!)
+  var tempList = [Int]()
+  for v in 1..<tempNums.count {
+    tempList.append(Int(tempNums[v])!)
+  }
+  lists.append(tempList)
+}
+
+var total1 = 0, total2 = 0
+for i in 0..<totals.count {
+  let x = Int(truncating: NSDecimalNumber(decimal: pow(2, lists[i].count-1)-1))
+  for j in 0...x {
+    var tempBin = Array(String(j, radix: 2))
+    while tempBin.count < lists[i].count-1 {
+      tempBin = "0" + tempBin
+    }
+    var tempTotal1 = lists[i][0]
+    for c in 0..<tempBin.count {
+      if tempBin[c] == "0" {
+        tempTotal1 += lists[i][c+1]
+      } else {
+        tempTotal1 *= lists[i][c+1]
+      }
+      if tempTotal1 > totals[i] {
+        break
+      }
+    }
+    if tempTotal1 == totals[i] {
+      total1 += totals[i]
+      break
+    } else {
+      tempTotal1 = lists[i][0]
+    }
+  }
+
+  let y = Int(truncating: NSDecimalNumber(decimal: pow(3, lists[i].count-1)-1))
+  for j in 0...y {
+    var tempTern = Array(String(j, radix: 3))
+    while tempTern.count < lists[i].count-1 {
+      tempTern = "0" + tempTern
+    }
+    var tempTotal2 = lists[i][0]
+    for c in 0..<tempTern.count {
+      if tempTern[c] == "0" {
+        tempTotal2 += lists[i][c+1]
+      } else if tempTern[c] == "1" {
+        tempTotal2 *= lists[i][c+1]
+      } else {
+        tempTotal2 = Int(String(tempTotal2) + String(lists[i][c+1]))!
+      }
+      if tempTotal2 > totals[i] {
+        break
+      }
+    }
+
+    if tempTotal2 == totals[i] {
+      total2 += totals[i]
+      break
+    } else {
+      tempTotal2 = lists[i][0]
+    }
+  }
+}
+
+print("Part 1 answer: \(total1)")
+print("Part 2 answer: \(total2)")
