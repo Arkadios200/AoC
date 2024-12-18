e677fef25a91dce64e8a9cf71b15cd8263131a72
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2024-12-17 18:09:27 -0600

    Update AoC24-17a.swift

---
+++ 2024/AoC24-17a.swift
@@ -0,0 +1,88 @@
+import Foundation
+
+func exp(_ a: Int, _ b: Int) -> Int {
+  if b == 0 {return 1}
+
+  var x = 1
+  for _ in 1...b {
+    x *= a
+  }
+
+  return x  
+}
+
+func getComboOp (_ literal: Int, _ regs: [Character: Int]) -> Int {
+  switch literal {
+    case 0...3:
+      return literal
+    case 4:
+      return regs["a"]!
+    case 5:
+      return regs["b"]!
+    case 6:
+      return regs["c"]!
+    default:
+      print("Something broke")
+      return -1
+  }
+}
+
+func runProgram(_ program: [Int], a: Int) -> String {
+  var regs: [Character: Int] = ["a": a, "b": 0, "c": 0]
+
+  var output = ""
+  var i = 0
+  while i < program.count {
+    let op = program[i+1]
+    switch program[i] {
+      case 0:
+        regs["a"] = regs["a"]! / exp(2, getComboOp(op, regs))
+        i += 2
+        break
+      case 1:
+        regs["b"] = regs["b"]! ^ op
+        i += 2
+        break
+      case 2:
+        regs["b"] = getComboOp(op, regs) % 8
+        i += 2
+        break
+      case 3:
+        i = regs["a"] != 0 ? op : i + 2
+        break
+      case 4:
+        regs["b"] = regs["b"]! ^ regs["c"]!
+        i += 2
+        break
+      case 5:
+        output.append("\(String(getComboOp(op, regs) % 8)),")
+        i += 2
+        break
+      case 6:
+        regs["b"] = regs["a"]! / exp(2, getComboOp(op, regs))
+        i += 2
+        break
+      case 7:
+        regs["c"] = regs["a"]! / exp(2, getComboOp(op, regs))
+        i += 2
+        break
+      default:
+        print("Something broke")
+       break
+    }
+  }
+  
+  output.remove(at: output.index(before: output.endIndex))
+
+  return output
+}
+
+let temp = "2,4,1,2,7,5,1,3,4,3,5,5,0,3,3,0"
+
+var input = [Int]()
+for t in temp.split(separator: ",") {
+  input.append(Int(t)!)
+}
+
+let total1 = runProgram(input, a: 64584136)
+print("Part 1 answer: \(total1)")
