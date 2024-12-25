64f53549b79d96cd9901609c6d9af28faccf38f7
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2024-12-25 00:57:59 -0600

    Update AoC16-03ab.swift

---
+++ 2016/AoC16-03ab.swift
@@ -0,0 +1,34 @@
+func numberOfValidTriangles(in input: [[Int]]) -> Int {
+  var total = 0
+
+  for i in input {
+    var line = i
+    let m = i.max()!
+
+    for j in 0..<3 where line[j] == m {
+      line.remove(at: j)
+      break
+    }
+
+    total += (line[0] + line[1]) > m ? 1 : 0
+  }
+
+  return total
+}
+
+var input1 = [[Int]]()
+while let line = readLine() {
+  input1.append(line.split(separator: " ").map( { Int($0)! } ))
+}
+
+var input2 = [[Int]]()
+for i in stride(from: 0, to: input1.count, by: 3) {
+  for j in 0..<3 {
+    input2.append([input1[i][j], input1[i+1][j], input1[i+2][j]])
+  }
+}
+
+let total1 = numberOfValidTriangles(in: input1)
+print("Part 1 answer: \(total1)")
+let total2 = numberOfValidTriangles(in: input2)
+print("Part 2 answer: \(total2)")
