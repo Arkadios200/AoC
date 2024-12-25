5903aac71e8b5bbafbb32b68708068bc64889d36
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2024-12-25 12:02:38 -0600

    Create AoC16-05ab.py

---
+++ 2016/AoC16-05ab.py
@@ -0,0 +1,21 @@
+import hashlib
+
+i = 0
+input = "wtnhxymk"
+code1 = ""
+code2 = [" ", " ", " ", " ", " ", " ", " ", " "]
+while (len(code1) < 8 or " " in code2):
+  string = input + str(i)
+  result = hashlib.md5(string.encode()).hexdigest()
+  if result[0:5] == "00000":
+    if len(code1) < 8:
+      code1 += result[5]
+
+    if result[5] >= "0" and result[5] <= "7":
+      if code2[int(result[5])] == " ":
+        code2[int(result[5])] = result[6]
+  
+  i += 1
+
+print(code1)
+print("".join(code2))
