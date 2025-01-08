239ded73ec2d093d086fa1d6ab4df2ab29337cb9
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-01-08 12:38:33 -0800

    Update AoC20-05ab.swift

--- 2020/AoC20-05ab.swift
+++ 2020/AoC20-05ab.swift
@@ -1,20 +1,7 @@
-extension Array {
-  func split(at index: Index) -> (Array, Array) {
-    return (Array(self[0..<index]), Array(self[index..<self.count]))
-  }
-}
-
-func getSeatID(of pass: String) -> Int {
-  let temp = Array(pass).split(at: 7)
-  let row = Int(temp.0.map( { ($0 == "B" ? "1" : "0") } ).joined(), radix: 2)!
-  let col = Int(temp.1.map( { ($0 == "R" ? "1" : "0") } ).joined(), radix: 2)!
-
-  return (row * 8) + col
-}
-
 var seats = [Int]()
 while let line = readLine() {
-  seats.append(getSeatID(of: line))
+  let id = Int(line.map( { ($0 == "B" || $0 == "R") ? "1" : "0" } ).joined(), radix: 2)!
+  seats.append(id)
 }
 
 var total1 = seats.max()!
