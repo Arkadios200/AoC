03fd70e924eebb11eded8488a02e036a643c0f04
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-01-29 17:20:41 -0500

    Update AoC15-02ab.swift

---
+++ 2015/AoC15-02ab.swift
@@ -0,0 +1,23 @@
+func val1(of line: [Int]) -> Int {
+  var val = line[0] * line[1]
+  for i in 0..<3 {
+    val += 2 * line[i] * line[(i+1) % 3]
+  }
+
+  return val
+}
+
+func val2(of line: [Int]) -> Int {
+  return 2*line[0] + 2*line[1] + line.reduce(1, *)
+}
+
+var total1 = 0, total2 = 0
+while let line = readLine() {
+  let temp = line.split(separator: "x").map( { Int($0)! } ).sorted()
+
+  total1 += val1(of: temp)
+  total2 += val2(of: temp)
+}
+
+print("Part 1 answer: \(total1)")
+print("Part 2 answer: \(total2)")
