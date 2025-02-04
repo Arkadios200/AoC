b232f36b39d4cefa5a26024cb79bd03f53466cc1
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-02-03 19:27:18 -0500

    Update AoC16-06ab.swift

--- 2016/AoC16-06ab.swift
+++ 2016/AoC16-06ab.swift
@@ -10,8 +10,8 @@ for i in 0..<input[0].count {
     let c = input[j][i]
     counts[c] = (counts[c] ?? 0) + 1
   }
-  out1.append(counts.max(by: { $0.value < $1.value } )!.key)
-  out2.append(counts.min(by: { $0.value < $1.value } )!.key)
+  out1.append((counts.max(by: { $0.value < $1.value } )?.key) ?? "?")
+  out2.append((counts.min(by: { $0.value < $1.value } )?.key) ?? "?")
 }
 
 print("Part 1 answer: \(out1)")
