3e0b08ba05870dde52bbd717a217ebadf23dcd40
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-02-06 09:32:08 -0500

    Update AoC18-10ab.swift

--- 2018/AoC18-10ab.swift
+++ 2018/AoC18-10ab.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 struct Light {
-  var pos: (x: Int, y: Int)
-  let vel: (x: Int, y: Int)
+  private var pos: (x: Int, y: Int)
+  private let vel: (x: Int, y: Int)
 
   init(_ nums: [Int]) {
     pos = (nums[0], nums[1])
@@ -13,35 +13,35 @@ struct Light {
     pos.x += vel.x
     pos.y += vel.y
   }
-}
 
-func skyMap(of lights: [Light]) -> [[Character]]? {
-  let width = 1 + lights.max(by: { $0.pos.x < $1.pos.x } )!.pos.x - lights.min(by: { $0.pos.x < $1.pos.x } )!.pos.x
-  let height = 1 + lights.max(by: { $0.pos.y < $1.pos.y } )!.pos.y - lights.min(by: { $0.pos.y < $1.pos.y } )!.pos.y
+  static func skyMap(of lights: [Light]) -> [[Character]]? {
+    let width = 1 + lights.max(by: { $0.pos.x < $1.pos.x } )!.pos.x - lights.min(by: { $0.pos.x < $1.pos.x } )!.pos.x
+    let height = 1 + lights.max(by: { $0.pos.y < $1.pos.y } )!.pos.y - lights.min(by: { $0.pos.y < $1.pos.y } )!.pos.y
 
-  if height != lights.count == 30 ? 8 : 10 {
-    return nil
+    if height != 10 {
+      return nil
+    }
+
+    var map = [[Character]](repeating: [Character](repeating: " ", count: width), count: height)
+    for light in lights {
+      let i = light.pos.y - lights.min(by: { $0.pos.y < $1.pos.y } )!.pos.y
+      let j = light.pos.x - lights.min(by: { $0.pos.x < $1.pos.x } )!.pos.x
+
+      map[i][j] = "█"
+    }
+
+    return map
   }
-
-  var map = [[Character]](repeating: [Character](repeating: " ", count: width), count: height)
-  for light in lights {
-    let i = light.pos.y - lights.min(by: { $0.pos.y < $1.pos.y } )!.pos.y
-    let j = light.pos.x - lights.min(by: { $0.pos.x < $1.pos.x } )!.pos.x
-
-    map[i][j] = "█"
-  }
-
-  return map
 }
 
 var lights: [Light] = (try? String(contentsOf: URL(fileURLWithPath: "input.txt")))!
-  .split(separator: "\n")
-  .map( { Light(
-    $0
-    .filter( { " -1234567890".contains($0) } )
-    .split(separator: " ")
-    .map( { Int($0)! } )
-  ) } )
+.split(separator: "\n")
+.map( { Light(
+  $0
+  .filter( { " -1234567890".contains($0) } )
+  .split(separator: " ")
+  .map( { Int($0)! } )
+) } )
 
 var count = 0
 while true {
@@ -50,7 +50,7 @@ while true {
     lights[i].step()
   }
 
-  if let map = skyMap(of: lights) {
+  if let map = Light.skyMap(of: lights) {
     print("Part 1 answer:")
     map.forEach( { print(String($0)) } )
     break
