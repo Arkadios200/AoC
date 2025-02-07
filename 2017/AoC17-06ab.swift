032b4161b3c9d95e517da7d384aa4cfc3680056e
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-02-07 17:43:37 -0500

    Update AoC17-06ab.swift

--- 2017/AoC17-06ab.swift
+++ 2017/AoC17-06ab.swift
@@ -1,31 +1,26 @@
-var banks = readLine()!.split(separator: "\t").map( { Int($0)! } )
+func redistribute(_ banks: inout [Int]) -> Int {
+  var record = [banks]
 
-var record = [[Int]]()
-record.append(banks)
-
-var check = false
-while true {
-  for i in 0..<banks.count where banks[i] == banks.max()! {
-    var iter = i
-    var val = banks[i]
-    banks[i] = 0
-    while val > 0 {
-      iter = (iter + 1) % banks.count
-      banks[iter] += 1
-      val -= 1
+  while true {
+    for (i, e) in banks.enumerated() where e == banks.max()! {
+      var iter = i
+      banks[i] = 0
+      for _ in 1...e {
+        iter = (iter + 1) % banks.count
+        banks[iter] += 1
+      }
+      break
     }
-    break
-  }
 
-  if !record.contains(where: { $0 == banks }) {
-    record.append(banks)
-  } else {
-    if check == true {break}
-    print("Part 1 answer: \(record.count)")
-    record.removeAll()
-    record.append(banks)
-    check = true
+    if record.contains(banks) {
+      return record.count
+    } else {
+      record.append(banks)
+    }
   }
 }
 
-print("Part 2 answer: \(record.count)")
+var banks = readLine()!.split(separator: "\t").map( { Int($0)! } )
+
+print("Part 1 answer: \(redistribute(&banks))")
+print("Part 2 answer: \(redistribute(&banks))")
