365fd9c43f0796c55f0d737d074a1c85b2a786d6
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-02-15 15:51:37 -0600

    Update AoC24-02ab.swift

--- 2024/AoC24-02ab.swift
+++ 2024/AoC24-02ab.swift
@@ -1,35 +1,33 @@
 struct Report {
   var levels: [Int]
+  var isSafe: Bool {
+    get {
+      let inc = (levels[0] <= levels[1])
+      return levels.indices[1...].allSatisfy( {
+        let diff = (levels[$0] - levels[$0-1]) * (inc ? 1 : -1)
+        return (1...3).contains(diff)
+      } )
+    }
+  }
   
   init(_ levels: [Int]) {
     self.levels = levels
   }
-
-  func isSafe() -> Bool {
-    let inc = (levels[0] <= levels[1])
-    return levels.indices[1...].allSatisfy( {
-      let diff = (levels[$0] - levels[$0-1]) * (inc ? 1 : -1)
-      return (1...3).contains(diff)
-    } )
-  }
 }
 
 func getTotal1(of reports: [Report]) -> Int {
-  return reports.filter( { $0.isSafe() } ).count
+  return reports.filter( { $0.isSafe } ).count
 }
 
 func getTotal2(of reports: [Report]) -> Int {
   return reports.filter( {
-    var safe = $0.isSafe()
-    if !safe {
-      for i in 0..<$0.levels.count {
-        var temp = $0
-        temp.levels.remove(at: i)
-        safe = temp.isSafe()
-        if safe { break }
-      }
+    if $0.isSafe { return true }
+    for i in $0.levels.indices {
+      var temp = $0
+      temp.levels.remove(at: i)
+      if temp.isSafe { return true }
     }
-    return safe
+    return false
   } ).count
 }
 
@@ -38,8 +36,5 @@ while let line = readLine() {
   reports.append(Report(line.split(separator: " ").map( { Int(String($0))! } )))
 }
 
-
-let total1 = getTotal1(of: reports)
-print("Part 1 answer: \(total1)")
-let total2 = getTotal2(of: reports)
-print("Part 2 answer: \(total2)")
+print("Part 1 answer: \(getTotal1(of: reports))")
+print("Part 2 answer: \(getTotal2(of: reports))")
