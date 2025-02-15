3e80053a3e7dc4f1c4851f50bdd923189960d24c
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-02-15 16:11:34 -0600

    Update AoC24-01ab.swift

--- 2024/AoC24-01ab.swift
+++ 2024/AoC24-01ab.swift
@@ -1,3 +1,7 @@
+func + (a: (Int, Int), b: (Int, Int)) -> (Int, Int) {
+  return (a.0 + b.0, a.1 + b.1)
+}
+
 var lists: ([Int], [Int]) = ([], [])
 while let line = readLine() {
   let temp = line.split(separator: " ").map( { Int($0)! } )
@@ -8,16 +12,10 @@ while let line = readLine() {
 lists.0.sort()
 lists.1.sort()
 
-var total1 = 0
-var total2 = 0
-for i in 0..<lists.0.count {
-  total1 += abs(lists.1[i] - lists.0[i])
+let (total1, total2) = lists.0.indices.reduce((0, 0), {
+  let i = $1
+  return $0 + (abs(lists.1[i] - lists.0[i]), lists.0[i] * lists.1.filter( { $0 == lists.0[i] } ).count)
+} )
 
-  var count = 0
-  for j in lists.1 where lists.0[i] == j {
-    count += 1
-  }
-  total2 += lists.0[i] * count
-}
 print("Part 1 answer: \(total1)")
 print("Part 2 answer: \(total2)")
