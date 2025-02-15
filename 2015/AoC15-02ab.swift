79c02fa14e87a5a4fb3a98168e65a354e950daf5
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-02-15 13:21:01 -0600

    Update AoC15-02ab.swift

--- 2015/AoC15-02ab.swift
+++ 2015/AoC15-02ab.swift
@@ -1,23 +1,21 @@
-func val1(of line: [Int]) -> Int {
-  var val = line[0] * line[1]
-  for i in 0..<3 {
-    val += 2 * line[i] * line[(i+1) % 3]
-  }
-
-  return val
+func += (a: inout (Int, Int), b: (Int, Int)) {
+  a.0 += b.0
+  a.1 += b.1
 }
 
-func val2(of line: [Int]) -> Int {
-  return 2*(line[0] + line[1]) + line.reduce(1, *)
+func val(of line: [Int]) -> (Int, Int) {
+  let val1 = line[0] * line[1] + line.indices.reduce(0, { $0 + 2 * line[$1] * line[($1 + 1) % 3] } )
+
+  let val2 = 2*(line[0] + line[1]) + line.reduce(1, *)
+
+  return (val1, val2)
 }
 
-var total1 = 0, total2 = 0
+var total = (0, 0)
 while let line = readLine() {
-  let temp = line.split(separator: "x").map( { Int($0)! } ).sorted()
-
-  total1 += val1(of: temp)
-  total2 += val2(of: temp)
+  total += val(of: line.split(separator: "x").map( { Int($0)! } ).sorted())
 }
 
+let (total1, total2) = total
 print("Part 1 answer: \(total1)")
 print("Part 2 answer: \(total2)")
