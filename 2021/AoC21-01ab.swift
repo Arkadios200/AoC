fbcea0c91c196d0f36235ec8bb9c51e2082c6436
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-02-16 00:39:15 -0600

    Update AoC21-01ab.swift

--- 2021/AoC21-01ab.swift
+++ 2021/AoC21-01ab.swift
@@ -7,6 +7,5 @@ let total1 = m1.indices[1...].filter( { m1[$0-1] < m1[$0] } ).count
 print("Part 1 answer: \(total1)")
 
 let m2 = m1.indices[2...].map( { m[$0-2...$0].reduce(0, +) } )
-
 let total2 = m2.indices[1...].filter( { m2[$0-1] < m2[$0] } ).count
 print("Part 2 answer: \(total2)")
