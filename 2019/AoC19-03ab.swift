d1bd667b407233ea98e520f285566db5daf9d11f
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-02-19 05:51:53 -0600

    Update AoC19-03ab.swift

--- 2019/AoC19-03ab.swift
+++ 2019/AoC19-03ab.swift
@@ -12,40 +12,39 @@ func part1(_ lines: [[Character]]) -> (Int, Int) {
   return (Int(gamma, radix: 2)!, Int(epsilon, radix: 2)!)
 }
 
-func oxygenGeneratorRating(_ lines: [[Character]]) -> Int {
-  var temp = lines
+func part2(_ lines: [[Character]]) -> (Int, Int) {
+  var tempOGR = lines
+  var tempCSR = lines
   for i in lines[0].indices {
     var count = 0
-    temp.forEach( {
-      count += $0[i] == "1" ? 1 : -1
-    } )
-    temp = temp.filter( { $0[i] == (count >= 0 ? "1" : "0") } )
-    if temp.count == 1 { break }
+
+    if tempOGR.count > 1 {
+      tempOGR.forEach( {
+        count += $0[i] == "1" ? 1 : -1
+      } )
+      tempOGR = tempOGR.filter( { $0[i] == (count >= 0 ? "1" : "0") } )
+    }
+
+    if tempCSR.count > 1 {
+      count = 0
+      tempCSR.forEach( {
+        count += $0[i] == "1" ? 1 : -1
+      } )
+      tempCSR = tempCSR.filter( { $0[i] == (count >= 0 ? "0" : "1") } )
+    }
   }
 
-  return Int(String(temp.first!), radix: 2)!
-}
-
-func CO2ScrubberRating(_ lines: [[Character]]) -> Int {
-  var temp = lines
-  for i in lines[0].indices {
-    var count = 0
-    temp.forEach( {
-      count += $0[i] == "1" ? 1 : -1
-    } )
-    temp = temp.filter( { $0[i] == (count >= 0 ? "0" : "1") } )
-    if temp.count == 1 { break }
-  }
-
-  return Int(String(temp.first!), radix: 2)!
+  return (Int(String(tempOGR.first!), radix: 2)!, Int(String(tempCSR.first!), radix: 2)!)
 }  
 
 var lines = [[Character]]()
 while let line = readLine() {
+  if line == "" { break }
   lines.append(Array(line))
 }
 
 let (gamma, epsilon) = part1(lines)
 print("Part 1 answer: \(gamma * epsilon)")
 
-print("Part 2 answer: \(oxygenGeneratorRating(lines) * CO2ScrubberRating(lines))")
+let (oxygenGeneratorRating, CO2ScrubberRating) = part2(lines)
+print("Part 2 answer: \(oxygenGeneratorRating * CO2ScrubberRating)")
