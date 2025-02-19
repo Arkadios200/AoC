b8774f83e46494faacde1883ecceee817c2ee591
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-02-19 06:06:55 -0600

    Update AoC19-03ab.swift

--- 2019/AoC19-03ab.swift
+++ 2019/AoC19-03ab.swift
@@ -1,40 +1,46 @@
+extension String {
+  var parseBin: Int? {
+    return Int(self, radix: 2)
+  }
+}
+
+func getCount(_ lines: [[Character]], _ i: Int) -> Int {
+  var count = 0
+  lines.forEach( {
+    count += $0[i] == "1" ? 1 : -1
+  } )
+
+  return count
+}
+
 func part1(_ lines: [[Character]]) -> (Int, Int) {
   var gamma = "", epsilon = ""
   for i in lines[0].indices {
-    var count = 0
-    lines.forEach( {
-      count += $0[i] == "1" ? 1 : -1
-    } )
+    let count = getCount(lines, i)
 
     gamma.append(count >= 0 ? "1" : "0")
     epsilon.append(count >= 0 ? "0" : "1")
   }
-  return (Int(gamma, radix: 2)!, Int(epsilon, radix: 2)!)
+
+  return (gamma.parseBin!, epsilon.parseBin!)
 }
 
 func part2(_ lines: [[Character]]) -> (Int, Int) {
   var tempOGR = lines
   var tempCSR = lines
   for i in lines[0].indices {
-    var count = 0
-
     if tempOGR.count > 1 {
-      tempOGR.forEach( {
-        count += $0[i] == "1" ? 1 : -1
-      } )
-      tempOGR = tempOGR.filter( { $0[i] == (count >= 0 ? "1" : "0") } )
+      tempOGR = tempOGR.filter( { $0[i] == (getCount(tempOGR, i) >= 0 ? "1" : "0") } )
     }
 
     if tempCSR.count > 1 {
-      count = 0
-      tempCSR.forEach( {
-        count += $0[i] == "1" ? 1 : -1
-      } )
-      tempCSR = tempCSR.filter( { $0[i] == (count >= 0 ? "0" : "1") } )
+      tempCSR = tempCSR.filter( { $0[i] == (getCount(tempCSR, i) >= 0 ? "0" : "1") } )
     }
+
+    if tempOGR.count == 1 && tempCSR.count == 1 { break }
   }
 
-  return (Int(String(tempOGR.first!), radix: 2)!, Int(String(tempCSR.first!), radix: 2)!)
+  return (String(tempOGR.first!).parseBin!, String(tempCSR.first!).parseBin!)
 }  
 
 var lines = [[Character]]()
