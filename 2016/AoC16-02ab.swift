f333b5678f7744feb7b83dd8f315638adc56d2d3
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-02-22 03:55:27 -0600

    Update AoC16-02ab.swift

--- 2016/AoC16-02ab.swift
+++ 2016/AoC16-02ab.swift
@@ -1,69 +1,56 @@
-let keypad1: [[String]] = [
-  ["1", "2", "3"],
-  ["4", "5", "6"],
-  ["7", "8", "9"]
+let keypad1: [[Character]] = [
+  [" ", " ", " ", " ", " "],
+  [" ", "1", "2", "3", " "],
+  [" ", "4", "5", "6", " "],
+  [" ", "7", "8", "9", " "],
+  [" ", " ", " ", " ", " "]
 ]
-var pos1 = (1, 1)
+var pos1 = (2, 2)
 
-let keypad2: [[String]] = [
-  [" ", " ", "1", " ", " "],
-  [" ", "2", "3", "4", " "],
-  ["5", "6", "7", "8", "9"],
-  [" ", "A", "B", "C", " "],
-  [" ", " ", "D", " ", " "]
+let keypad2: [[Character]] = [
+  [" ", " ", " ", " ", " ", " ", " "],
+  [" ", " ", " ", "1", " ", " ", " "],
+  [" ", " ", "2", "3", "4", " ", " "],
+  [" ", "5", "6", "7", "8", "9", " "],
+  [" ", " ", "A", "B", "C", " ", " "],
+  [" ", " ", " ", "D", " ", " ", " "],
+  [" ", " ", " ", " ", " ", " ", " "]
 ]
-var pos2 = (2, 0)
+var pos2 = (3, 1)
 
-var code1 = [String]()
-var code2 = [String]()
+var code1 = ""
+var code2 = ""
 while let line = readLine() {
   for c in line {
+    var (i, j) = pos1
     switch c {
-      case "U" where pos1.0 > 0:
+      case "U" where keypad1[i-1][j] != " ":
         pos1.0 -= 1
-        break
-      case "D" where pos1.0 < 2:
+      case "D" where keypad1[i+1][j] != " ":
         pos1.0 += 1
-        break
-      case "L" where pos1.1 > 0:
+      case "L" where keypad1[i][j-1] != " ":
         pos1.1 -= 1
-        break
-      case "R" where pos1.1 < 2:
+      case "R" where keypad1[i][j+1] != " ":
         pos1.1 += 1
-        break
-      default:
-        break
+      default: break
     }
 
+    (i, j) = pos2
     switch c {
-      case "U" where pos2.0 > 0:
-        if keypad2[pos2.0-1][pos2.1] != " " {
-          pos2.0 -= 1
-        }
-        break
-      case "D" where pos2.0 < 4:
-        if keypad2[pos2.0+1][pos2.1] != " " {
-          pos2.0 += 1
-        }
-        break
-      case "L" where pos2.1 > 0:
-        if keypad2[pos2.0][pos2.1-1] != " " {
-          pos2.1 -= 1
-        }
-        break
-      case "R" where pos2.1 < 4:
-        if keypad2[pos2.0][pos2.1+1] != " " {
-          pos2.1 += 1
-        }
-        break
-      default:
-        break
+      case "U" where keypad2[i-1][j] != " ":
+        pos2.0 -= 1
+      case "D" where keypad2[i+1][j] != " ":
+        pos2.0 += 1
+      case "L" where keypad2[i][j-1] != " ":
+        pos2.1 -= 1
+      case "R" where keypad2[i][j+1] != " ":
+        pos2.1 += 1
+      default: break
     }
   }
-
   code1.append(keypad1[pos1.0][pos1.1])
   code2.append(keypad2[pos2.0][pos2.1])
 }
 
-print("Part 1 answer: \(code1.joined())")
-print("Part 2 answer: \(code2.joined())")
+print(code1)
+print(code2)
