192982b21470948060e68f466a0cb4dbbc3405d1
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-02-28 13:51:31 -0600

    Update AoC15-16ab.swift

--- 2015/AoC15-16ab.swift
+++ 2015/AoC15-16ab.swift
@@ -11,35 +11,37 @@ struct Aunt {
 func process(_ line: String) -> (Int, [(String, Int)]) {
   let temp = line.filter( { $0 != " " } ).split(separator: ":", maxSplits: 1)
   let num = Int(temp[0].filter( { $0.isNumber } ))!
-  let data = temp[1].split(separator: ",").map( { $0.split(separator: ":") } ).map( { (String($0[0]), Int($0[1])!) } )
+  let data = temp[1].split(separator: ",")
+    .map( { $0.split(separator: ":") } )
+    .map( { (String($0[0]), Int($0[1])!) } )
 
   return (num, data)
 }
 
 let targetAunt1: [String: Int] = [
-  "children": 3,
-  "cats": 7,
-  "samoyeds": 2,
+  "children":    3,
+  "cats":        7,
+  "samoyeds":    2,
   "pomeranians": 3,
-  "akitas": 0,
-  "vizslas": 0,
-  "goldfish": 5,
-  "trees": 3,
-  "cars": 2,
-  "perfumes": 1
+  "akitas":      0,
+  "vizslas":     0,
+  "goldfish":    5,
+  "trees":       3,
+  "cars":        2,
+  "perfumes":    1
 ]
 
 let targetAunt2: [String: (Int) -> Bool] = [
-  "children": { $0 == 3 },
-  "cats": { $0 > 7 },
-  "samoyeds": { $0 == 2 },
+  "children":    { $0 == 3 },
+  "cats":        { $0 > 7 },
+  "samoyeds":    { $0 == 2 },
   "pomeranians": { $0 < 3 },
-  "akitas": { $0 == 0 },
-  "vizslas": { $0 == 0 },
-  "goldfish": { $0 < 5 },
-  "trees": { $0 > 3 },
-  "cars": { $0 == 2 },
-  "perfumes": { $0 == 1 }
+  "akitas":      { $0 == 0 },
+  "vizslas":     { $0 == 0 },
+  "goldfish":    { $0 < 5 },
+  "trees":       { $0 > 3 },
+  "cars":        { $0 == 2 },
+  "perfumes":    { $0 == 1 }
 ]
 
 var aunts = [Aunt]()
@@ -49,23 +51,11 @@ while let line = readLine() {
 }
 
 let ans1 = aunts.first(where: {
-  (aunt: Aunt) in
-  for line in aunt.data {
-    if targetAunt1[line.0]! != line.1 {
-      return false
-    }
-  }
-  return true
+  $0.data.allSatisfy( { targetAunt1[$0.0]! == $0.1 } )
 } )!.num
 
 let ans2 = aunts.first(where: {
-  (aunt: Aunt) in
-  for line in aunt.data {
-    if !targetAunt2[line.0]!(line.1) {
-      return false
-    }
-  }
-  return true
+  $0.data.allSatisfy( { targetAunt2[$0.0]!($0.1) } )
 } )!.num
 
 print("Part 1 answer: \(ans1)")
