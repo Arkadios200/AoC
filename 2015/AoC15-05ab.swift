d1ff9a94037741060ffb7c327cc1588341a79fa4
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-02-28 23:48:10 -0600

    Update AoC15-05ab.swift

--- 2015/AoC15-05ab.swift
+++ 2015/AoC15-05ab.swift
@@ -12,15 +12,19 @@ func nice1(_ s: String) -> Bool {
 func nice2(_ s: String) -> Bool {
   let indices = s.indices
 
-  let cond1 = indices[..<indices.index(indices.endIndex, offsetBy: -3)].contains(where: { indices[indices.index($0, offsetBy: 2)..<indices.index(before: indices.endIndex)].map( { s[$0...s.index(after: $0)] } ).contains(s[$0...s.index(after: $0)]) } )
+  let cond1 = indices[..<indices.index(indices.endIndex, offsetBy: -3)]
+    .contains(where: {
+      indices[indices.index($0, offsetBy: 2)..<indices.index(before: indices.endIndex)]
+        .map( { s[$0...s.index(after: $0)] } )
+        .contains(s[$0...s.index(after: $0)])
+    } )
   
   let cond2 = indices[..<indices.index(indices.endIndex, offsetBy: -2)].contains(where: { s[$0] == s[s.index($0, offsetBy: 2)] } )
   
   return cond1 && cond2
 }
 
-let input = (try? String(contentsOf: URL(fileURLWithPath: "input.txt")))!.split(separator: "\n").map( { String($0) } )
+let input = (try! String(contentsOf: URL(fileURLWithPath: "input.txt"))).split(separator: "\n").map( { String($0) } )
 
 print("Part 1 answer: \(input.filter( { nice1($0) } ).count)")
-
 print("Part 2 answer: \(input.filter( { nice2($0) } ).count)")
