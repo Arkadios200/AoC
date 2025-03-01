55657ce4ea0ee7a2b3d5c91d361f1fc73654fafa
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-02-28 20:15:48 -0600

    Create AoC15-16ab.rs

---
+++ 2015/AoC15-16ab.rs
@@ -0,0 +1,73 @@
+use std::fs;
+use std::collections::HashMap;
+
+struct Aunt {
+  num: u32,
+  data: Vec<(String, u32)>
+}
+
+impl From<&str> for Aunt {
+  fn from(line: &str) -> Aunt {
+    let l = line.split_whitespace().collect::<String>();
+    let (a, b) = l.split_once(':').unwrap();
+
+    let num: u32 = a.chars()
+      .filter(|c| c.is_digit(10)).collect::<String>()
+      .parse().unwrap();
+
+    let data: Vec<(String, u32)> = b.split(',')
+      .map(|s| {
+        let (key, value) = s.split_once(':').unwrap();
+        (String::from(key), value.parse().unwrap())
+      }).collect();
+
+    Aunt { num, data }
+  }
+}
+
+fn main() {
+  let conds: Vec<Box<dyn Fn(u32) -> bool>> = vec![
+    Box::new(|x| x == 3),
+    Box::new(|x| x  > 7),
+    Box::new(|x| x == 2),
+    Box::new(|x| x  < 3),
+    Box::new(|x| x == 0),
+    Box::new(|x| x == 0),
+    Box::new(|x| x  < 5),
+    Box::new(|x| x  > 3),
+    Box::new(|x| x == 2),
+    Box::new(|x| x == 1)
+  ];
+  
+  let target_data = HashMap::from([
+    ("children",    (3, &conds[0])),
+    ("cats",        (7, &conds[1])),
+    ("samoyeds",    (2, &conds[2])),
+    ("pomeranians", (3, &conds[3])),
+    ("akitas",      (0, &conds[4])),
+    ("vizslas",     (0, &conds[5])),
+    ("goldfish",    (5, &conds[6])),
+    ("trees",       (3, &conds[7])),
+    ("cars",        (2, &conds[8])),
+    ("perfumes",    (1, &conds[9]))
+  ]);
+
+  
+  let input = fs::read_to_string("input.txt").unwrap();
+  let lines: Vec<_> = input.split('\n').collect();
+
+  let mut aunts: Vec<Aunt> = Vec::new();
+  for line in lines {
+    aunts.push(Aunt::from(line));
+  }
+
+  let ans1 = aunts.iter().find(|aunt|
+    aunt.data.iter().all(|(key, value)| target_data[key.as_str()].0 == *value)
+  ).unwrap().num;
+  println!("Part 1 answer: {ans1}");
+
+  let ans2 = aunts.iter().find(|aunt|
+    aunt.data.iter().all(|(key, value)| target_data[key.as_str()].1(*value))
+  ).unwrap().num;
+  println!("Part 2 answer: {ans2}");
+}
