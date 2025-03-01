259ea25446a50fe1a95e007a60abba32ff4c1d28
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-03-01 00:15:21 -0600

    Update AoC17-04ab.swift

--- 2017/AoC17-04ab.swift
+++ 2017/AoC17-04ab.swift
@@ -1,39 +1,20 @@
 func getContents(of s: String) -> [Character: Int] {
-  var contents = [Character: Int]()
-  for i in s.indices {
-    contents[s[i]] = 1 + (contents[s[i]] != nil ? contents[s[i]]! : 0) 
-  }
-
-  return contents
+  var counts = [Character: Int]()
+  s.forEach( { counts[$0] = (counts[$0] ?? 0) + 1 } )
+  return counts
 }
 
-func anagramChecker(_ a: String, _ b: String) -> Bool {
-  return getContents(of: a) == getContents(of: b)
+var passphrases = [[String]]()
+while let line = readLine() {
+  passphrases.append(line.split(separator: " ").map( { String($0) } ))
 }
 
-var total1 = 0, total2 = 0
-while let ln = readLine() {
-  var valid1 = true, valid2 = true
-  
-  let line = ln.split(separator: " ").map( { String($0) } )
-  running: for i in 0..<line.count-1 {
-    for j in i+1..<line.count {
-      if line[i] == line[j] {
-        valid1 = false
-      }
-      if anagramChecker(line[i], line[j]) {
-        valid2 = false
-      }
-      
-      if !(valid1 || valid2) {
-        break running
-      }
-    }
-  }
+let total1 = passphrases.filter( { $0.count == Set($0).count } ).count
 
-  total1 += valid1 ? 1 : 0
-  total2 += valid2 ? 1 : 0
-}
+let total2 = passphrases.filter( {
+  let temp = $0.map( { getContents(of: $0) } )
+  return temp.count == Set(temp).count
+} ).count
 
 print("Part 1 answer: \(total1)")
 print("Part 2 answer: \(total2)")
