093f3ca26037f0ce2c0cd7283d63f13055f1e13f
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-03-04 02:41:53 -0500

    Update AoC20-06ab.swift

--- 2020/AoC20-06ab.swift
+++ 2020/AoC20-06ab.swift
@@ -1,34 +1,27 @@
-var total1 = 0
-var total2 = 0
-var group = [String]()
-while let line = readLine() {
-  if line == "" {
-    var s = ""
-    part1: for g in group {
-      for i in g.indices where !s.contains(g[i]) {
-        s.append(g[i])
-      }
-    }
-    total1 += s.count
+import Foundation
 
-    s = ""
-    part2: for i in group[0].indices {
-      var foundInAll = true
-      for g in group {
-        if !g.contains(group[0][i]) {
-          foundInAll = false
-          break
-        }
-      }
-      if foundInAll { s.append(group[0][i]) }
-    }
-    total2 += s.count
-
-    group.removeAll()
-  } else {
-    group.append(line)
+func getGroups(from input: String) -> [[String]] {
+  let lines = input.split(separator: "\n", omittingEmptySubsequences: false).map( { String($0) } )
+  
+  var groups: [[String]] = [[]]
+  lines.forEach {
+    !$0.isEmpty ? groups[groups.indices.last!].append($0) : groups.append([])
   }
+
+  return groups
 }
+
+let input = try! String(contentsOf: URL(fileURLWithPath: "input.txt"))
+let groups = getGroups(from: input)
+
+let total1 = groups.map( { Set($0.joined()).count } ).reduce(0, +)
+
+let total2 = groups.map( {
+  g in
+  g.first!.filter( {
+    c in g.allSatisfy( { $0.contains(c) } )
+  } ).count
+} ).reduce(0, +)
 
 print("Part 1 answer: \(total1)")
 print("Part 2 answer: \(total2)")
