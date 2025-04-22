757bc36dfefe06864d3dc37b7348d15945f8f396
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-04-22 17:08:46 -0500

    Update AoC19-01ab.swift

--- 2019/AoC19-01ab.swift
+++ 2019/AoC19-01ab.swift
@@ -1,17 +1,30 @@
-let getFuel: (Int) -> Int = { ($0 / 3) - 2 }
-
-var total1 = 0, total2 = 0
-var modules = [Int]()
-while let line = readLine() {
-  var m = getFuel(Int(line)!)
-
-  total1 += m
-
-  while m > 0 {
-    total2 += m
-    m = getFuel(m)
-  }
+infix operator <==: AssignmentPrecedence
+func <== <T>(lhs: inout T, rhs: T) -> T {
+  lhs = rhs
+  return lhs
 }
 
+func getLines() -> [Int] {
+  var lines = [Int]()
+  while let line = readLine() {
+    lines.append(Int(line)!)
+  }
+
+  return lines
+}
+
+let getFuel: (Int) -> Int = { ($0 / 3) - 2 }
+
+let lines = getLines()
+
+let total1 = lines.reduce(0) { $0 + getFuel($1) }
 print("Part 1 answer: \(total1)")
-print("Part 2 answer: \(total2)")
+
+let total2 = lines.reduce(0) {
+  var n = $1
+  var v = 0
+  while (n <== getFuel(n)) > 0 { v += n }
+
+  return $0 + v
+}
+print("Part 1 answer: \(total2)")
