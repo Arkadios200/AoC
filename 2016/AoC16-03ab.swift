19ba25fc75a538e1527220e8cd8f269ff40d2d96
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-04-27 16:12:53 -0500

    Update AoC16-03ab.swift

--- 2016/AoC16-03ab.swift
+++ 2016/AoC16-03ab.swift
@@ -1,18 +1,29 @@
+func getLines() -> [[Int]] {
+  var input = [[Int]]()
+  while let line = readLine() {
+    input.append(line.split(separator: " ").map { Int($0)! })
+  }
+
+  return input
+}
+
+func convert(_ input: [[Int]]) -> [[Int]] {
+  var out = [[Int]]()
+  for i in input.indices.filter( { $0 % 3 == 0 } ) {
+    for j in 0..<3 {
+      out.append(input[i...i+2].map { $0[j] })
+    }
+  }
+
+  return out
+}
+
 func validTriangles(in input: [[Int]]) -> Int {
   return input.filter { (2 * $0.max()!) < $0.reduce(0, +) }.count
 }
 
-var input1 = [[Int]]()
-while let line = readLine() {
-  input1.append(line.split(separator: " ").map( { Int($0)! } ))
-}
-
-var input2 = [[Int]]()
-for i in input1.indices.filter( { $0 % 3 == 0 } ) {
-  for j in 0..<3 {
-    input2.append(input1[i...i+2].map { $0[j] })
-  }
-}
+let input1 = getLines()
+let input2 = convert(input1)
 
 print("Part 1 answer: \(validTriangles(in: input1))")
 print("Part 2 answer: \(validTriangles(in: input2))")
