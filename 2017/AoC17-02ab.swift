589d18823b49b5ada4f24c0b8c35ad8c57240e33
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-04-29 23:43:36 -0500

    Update AoC17-02ab.swift

--- 2017/AoC17-02ab.swift
+++ 2017/AoC17-02ab.swift
@@ -1,18 +1,25 @@
-var total1 = 0, total2 = 0
-while let x = readLine() {
-  let line = x.split(separator: "\t").map( { Int($0)! } )
-  total1 += line.max()! - line.min()!
+func getInput() -> [[Int]] {
+  var lines: [[Int]] = []
+  while let line = readLine() {
+    lines.append(line.split(separator: "\t").map { Int($0)! })
+  }
 
-  for i in 0..<line.count-1 {
-    for j in i+1..<line.count {
-      if line[i] % line[j] == 0 {
-        total2 += (line[i] / line[j])
-      } else if line[j] % line[i] == 0 {
-        total2 += (line[j] / line[i])
-      }
-    }
-  }       
+  return lines
 }
 
-print("Part 1 answer: \(total1)")
-print("Part 2 answer: \(total2)")
+let lines = getInput()
+
+let ans1 = lines.reduce(0) { $0 + $1.max()! - $1.min()! }
+print("Part 1 answer: \(ans1)")
+
+let ans2 = lines.reduce(0) {
+  (acc, line) in
+  for a in line {
+    if let b = line.first(where: { b in a % b == 0 && a != b } ) {
+      return acc + (a / b)
+    }
+  }
+  
+  return acc
+}
+print("Part 2 answer: \(ans2)")
