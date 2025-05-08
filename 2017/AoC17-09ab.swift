40e9842c2dee2c8666323f82be46c7ef4073925e
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-05-08 08:15:34 -0700

    Update AoC17-09ab.swift

--- 2017/AoC17-09ab.swift
+++ 2017/AoC17-09ab.swift
@@ -3,39 +3,28 @@ let stream = Array(readLine()!)
 var total1 = 0
 var total2 = 0
 var score = 0
-var i = 0
 var garbage = false
+var i = 0
+
 while i < stream.count {
   if !garbage {
     switch stream[i] {
-      case "{":
-        score += 1
-        break
+      case "{": score += 1
       case "}":
         total1 += score
         score -= 1
-        break
-      case "<":
-        garbage = true
-        break
-      default:
-        break
+      case "<": garbage = true
+      default: break
     }
   } else {
     switch stream[i] {
-      case "!":
-        i += 2
-        continue
-      case ">":
-        garbage = false
-        break
-      default:
-        total2 += 1
-        break
+      case "!": i += 1
+      case ">": garbage = false
+      default: total2 += 1
     }
   }
 
-  i += 1      
+  i += 1
 }
 
 print("Part 1 answer: \(total1)")
