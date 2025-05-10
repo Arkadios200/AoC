a0daeca94adca3e76f6adf19f61b0a22a98a7523
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-05-10 01:30:57 -0700

    Update AoC20-12ab.swift

--- 2020/AoC20-12ab.swift
+++ 2020/AoC20-12ab.swift
@@ -1,13 +1,3 @@
-func clockwise(_ a: inout (x: Int, y: Int)) {
-  let (b, c) = a
-  a = (c, -b)
-}
-
-func counterclockwise(_ a: inout (x: Int, y: Int)) {
-  let (b, c) = a
-  a = (-c, b)
-}
-
 func getInput() -> [(Character, Int)] {
   var lines: [(Character, Int)] = []
   while let line = readLine() {
@@ -18,25 +8,25 @@ func getInput() -> [(Character, Int)] {
   return lines
 }
 
+func step(_ pos: inout (x: Int, y: Int, dir: Int), _ dir: Character, _ dist: Int) {
+  switch dir {
+    case "N": pos.y += dist
+    case "S": pos.y -= dist
+    case "E": pos.x += dist
+    case "W": pos.x -= dist
+    case "R": pos.dir = (pos.dir + dist / 90) % 4
+    case "L": pos.dir = (pos.dir - (dist / 90) + 4) % 4
+    default: break
+  }
+}
+
 func navigate1(_ lines: [(Character, Int)]) -> Int {
   var pos = (x: 0, y: 0, dir: 1)
   for (dir, dist) in lines {
     switch dir {
-      case "N": pos.y += dist
-      case "S": pos.y -= dist
-      case "E": pos.x += dist
-      case "W": pos.x -= dist
-      case "R": pos.dir = (pos.dir + dist / 90) % 4
-      case "L": pos.dir = (pos.dir - (dist / 90) + 4) % 4
       case "F":
-        switch pos.dir {
-          case 0: pos.y += dist
-          case 1: pos.x += dist
-          case 2: pos.y -= dist
-          case 3: pos.x -= dist
-          default: break
-      }
-      default: break
+        step(&pos, ["N", "E", "S", "W"][pos.dir], dist)
+      default: step(&pos, dir, dist)
     }
   }
 
@@ -45,25 +35,27 @@ func navigate1(_ lines: [(Character, Int)]) -> Int {
 
 func navigate2(_ lines: [(Character, Int)]) -> Int {
   var pos = (x: 0, y: 0)
-  var waypoint = (x: 10, y: 1)
+  var wp = (x: 10, y: 1)
 
   for (dir, dist) in lines {
     switch dir {
-      case "N": waypoint.y += dist
-      case "S": waypoint.y -= dist
-      case "E": waypoint.x += dist
-      case "W": waypoint.x -= dist
+      case "N": wp.y += dist
+      case "S": wp.y -= dist
+      case "E": wp.x += dist
+      case "W": wp.x -= dist
       case "R":
         for _ in 1...(dist / 90) {
-          clockwise(&waypoint)
+          let (b, c) = wp
+          wp = (c, -b)
         }
       case "L":
         for _ in 1...(dist / 90) {
-          counterclockwise(&waypoint)
+          let (b, c) = wp
+          wp = (-c, b)
         }
       case "F":
-        pos.x += waypoint.x * dist
-        pos.y += waypoint.y * dist
+        pos.x += wp.x * dist
+        pos.y += wp.y * dist
       default: break
     }
   }
