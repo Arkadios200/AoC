ccff4f2c218dd0479f633e09dc0cab32aed12ed8
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-05-11 11:32:41 -0700

    Update AoC15-18ab.swift

--- 2015/AoC15-18ab.swift
+++ 2015/AoC15-18ab.swift
@@ -1,62 +1,62 @@
-// Needs improvement
+typealias Grid = [[Int]]
 
-func conwayStep(_ grid: [[Int]]) -> [[Int]] {
+func getInput() -> Grid {
+  var grid: Grid = []
+  while let line = readLine() {
+    grid.append(line.map { $0 == "#" ? 1 : 0 })
+  }
+
+  return grid
+}
+
+func step(_ grid: inout Grid) {
   var newGrid = grid
-  for i in grid.indices {
-    for j in grid[i].indices {
-      var count = 0
-      for k in -1...1 {
-        for l in -1...1 {
-          if grid.indices.contains(i + k) && grid[i].indices.contains(j + l) && !(k == 0 && l == 0) {
-            count += grid[i+k][j+l]
-          }
-        }
-      }
 
-      if grid[i][j] == 1 {
-        if !(2...3).contains(count) {
+  let gi = grid.indices
+  let gfi = grid.first!.indices
+  for i in gi {
+    let yRange = max(i-1, 0)...min(i+1, gi.last!)
+    for j in gfi {
+      let xRange = max(j-1, 0)...min(j+1, gfi.last!)
+
+      let temp = grid[yRange].map { $0[xRange] }.joined().reduce(0, +)
+      switch grid[i][j] {
+        case 1 where !(2...3).contains(temp - 1):
           newGrid[i][j] = 0
-        }
-      } else {
-        if count == 3 {
+        case 0 where temp == 3:
           newGrid[i][j] = 1
-        }
+        default: break
       }
     }
   }
 
-  return newGrid
+  grid = newGrid
 }
 
-var grid = [[Int]]()
-while let line = readLine() {
-  if line == "" { break }
-  grid.append(line.map( { $0 == "#" ? 1 : 0 } ))
-}
+let grid = getInput()
 
+let (sx, ex) = (grid.first!.indices.first!, grid.first!.indices.last!)
+let (sy, ey) = (grid.indices.first!, grid.indices.last!)
+ z 
 var grid1 = grid
-for _ in 1...100 {
-  grid1 = conwayStep(grid1)
-}
-
-var total1 = 0
-grid1.forEach( { total1 += $0.reduce(0, +) } )
-
-print(total1)
-
 var grid2 = grid
-grid2[0][0] = 1
-grid2[0][99] = 1
-grid2[99][0] = 1
-grid2[99][99] = 1
 for _ in 1...100 {
-  grid2 = conwayStep(grid2)
-  grid2[0][0] = 1
-  grid2[0][99] = 1
-  grid2[99][0] = 1
-  grid2[99][99] = 1
+  step(&grid1)
+
+  grid2[sy][sx] = 1
+  grid2[sy][ex] = 1
+  grid2[ey][sx] = 1
+  grid2[ey][ex] = 1
+  step(&grid2) ．
 }
 
-var total2 = 0
-grid2.forEach( { total2 += $0.reduce(0, +) } )
-print(total2)
+grid2[sy][sx] = 1
+grid2[sy][ex] = 1
+grid2[ey][sx] = 1
+grid2[ey][ex] = 1
+
+let ans1 = grid1.joined().reduce(0, +)
+print("Part 1 answer: \(ans1)")
+
+let ans2 = grid2.joined().reduce(0, +)
+print("Part 2 answer: \(ans2)")
