1fb14e6b5fa7b1d2980d46978340d52967539781
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-05-18 15:02:13 -0700

    Update AoC15-16ab.rs

--- 2015/AoC15-16ab.rs
+++ 2015/AoC15-16ab.rs
@@ -27,33 +27,33 @@ impl From<&str> for Aunt {
 }
 
 fn main() {
-  let conds: Vec<Box<dyn Fn(u32) -> bool>> = vec![
-    Box::new(|x| x == 3),
-    Box::new(|x| x  > 7),
-    Box::new(|x| x == 2),
-    Box::new(|x| x  < 3),
-    Box::new(|x| x == 0),
-    Box::new(|x| x == 0),
-    Box::new(|x| x  < 5),
-    Box::new(|x| x  > 3),
-    Box::new(|x| x == 2),
-    Box::new(|x| x == 1)
+  let conds: Vec<fn(u32) -> bool> = vec![
+    (|x| x == 3),
+    (|x| x  > 7),
+    (|x| x == 2),
+    (|x| x  < 3),
+    (|x| x == 0),
+    (|x| x == 0),
+    (|x| x  < 5),
+    (|x| x  > 3),
+    (|x| x == 2),
+    (|x| x == 1)
   ];
-  
+
   let target_data = HashMap::from([
-    ("children",    (3, &conds[0])),
-    ("cats",        (7, &conds[1])),
-    ("samoyeds",    (2, &conds[2])),
-    ("pomeranians", (3, &conds[3])),
-    ("akitas",      (0, &conds[4])),
-    ("vizslas",     (0, &conds[5])),
-    ("goldfish",    (5, &conds[6])),
-    ("trees",       (3, &conds[7])),
-    ("cars",        (2, &conds[8])),
-    ("perfumes",    (1, &conds[9]))
+    ("children",    (3, conds[0])),
+    ("cats",        (7, conds[1])),
+    ("samoyeds",    (2, conds[2])),
+    ("pomeranians", (3, conds[3])),
+    ("akitas",      (0, conds[4])),
+    ("vizslas",     (0, conds[5])),
+    ("goldfish",    (5, conds[6])),
+    ("trees",       (3, conds[7])),
+    ("cars",        (2, conds[8])),
+    ("perfumes",    (1, conds[9]))
   ]);
 
-  
+
   let input = fs::read_to_string("input.txt").unwrap();
   let aunts: Vec<Aunt> = input.split('\n').map(|line| Aunt::from(line)).collect();
 
