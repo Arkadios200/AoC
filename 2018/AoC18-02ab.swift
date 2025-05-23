a17f5fb26333634c238a5b1f765e93f1fd18242e
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-05-22 17:31:41 -0700

    Update AoC18-02ab.swift

--- 2018/AoC18-02ab.swift
+++ 2018/AoC18-02ab.swift
@@ -1,34 +1,43 @@
-var input = [[Character]]()
-while let line = readLine() {
-  input.append(Array(line))
-}
+extension Sequence where Element: Hashable {
+  var contents: [Element: Int] {
+    var contents: [Element: Int] = [:]
+    self.forEach { contents[$0] = (contents[$0] ?? 0) + 1 }
 
-var counts = [0, 0]
-for i in input {
-  var charCounts = [Character: Int]()
-  for c in i {
-    charCounts[c] = (charCounts[c] ?? 0) + 1
-  }
-
-  counts[0] += charCounts.values.contains(2) ? 1 : 0
-  counts[1] += charCounts.values.contains(3) ? 1 : 0
-}
-print("Part 1 answer: \(counts[0] * counts[1])")
-
-part2: for i in 0..<input.count-1 {
-  for j in i+1..<input.count {
-    var diff = 0
-    for k in 0..<input[i].count {
-      diff += (input[i][k] != input[j][k]) ? 1 : 0
-      if diff > 1 { break }
-    }
-    if diff == 1 {
-      var out = ""
-      for k in 0..<input[i].count where input[i][k] == input[j][k] {
-        out.append(input[j][k])
-      }
-      print("Part 2 answer: \(out)")
-      break part2
-    }
+    return contents
   }
 }
+
+extension Sequence where Element: Equatable {
+  func hammingDistance(from other: Self) -> Int {
+    return zip(self, other).filter { $0.0 != $0.1 }.count
+  }
+}
+
+func getInput() -> [String] {
+  var lines: [String] = []
+  while let line = readLine() {
+    lines.append(line)
+  }
+
+  return lines
+}
+
+func part2(_ lines: [String]) -> String? {
+  for (i, line) in lines.enumerated() {
+    if let s = lines.dropFirst(i+1).first(where: { $0.hammingDistance(from: line) == 1 } ) {
+      return String(zip(s, line).filter { $0.0 == $0.1 }.map { $0.0 })
+    }
+  }
+
+  return nil
+}
+
+let lines = getInput()
+
+let temp = lines.map { $0.contents.values }
+
+let ans1 = temp.filter { $0.contains(2) }.count * temp.filter { $0.contains(3) }.count
+print("Part 1 answer: \(ans1)")
+
+let ans2 = part2(lines)!
+print("Part 2 answer: \(ans2)")
