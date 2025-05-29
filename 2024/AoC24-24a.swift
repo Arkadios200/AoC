e91d30fe30620908efcd8c97c0c8887f0fa10992
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-05-29 05:58:47 -0700

    Update AoC24-24a.swift

--- 2024/AoC24-24a.swift
+++ 2024/AoC24-24a.swift
@@ -1,45 +1,48 @@
-import Foundation
+func getInput() -> ([String: Bool], [[String]]) {
+  var wires: [String: Bool] = [:]
+  while let line = readLine() {
+    if line.isEmpty { break }
+    let label = String(line.prefix { $0 != ":" })
+    wires[label] = (line.last! == "1")
+  }
 
-var wires = [String: Int]()
-while let line = readLine() {
-  if line == "" {break}
-  let temp = line.components(separatedBy: ": ")
-  wires[temp[0]] = Int(temp[1])!
+  var gates: [[String]] = []
+  while let line = readLine() {
+    gates.append(line.split(separator: " ").map { String($0) })
+  }
+
+  return (wires, gates)
 }
 
-var gates = [String]()
-while let line = readLine() {
-  gates.append(line)
-}
+func part1(_ wires: [String: Bool], _ gates: [[String]]) -> Int {
+  var wires = wires
 
-while gates.count > 0 {
-  for i in stride(from: gates.count-1, through: 0, by: -1) {
-    let g = gates[i].split(separator: " ").map( { String($0) } )
-    if let x = wires[g[0]], let y = wires[g[2]] {
-      switch g[1] {
-        case "AND":
-          wires[g[4]] = (x == 1 && y == 1) ? 1 : 0
-          break
-        case "OR":
-          wires[g[4]] = (x == 1 || y == 1) ? 1 : 0
-          break
-        case "XOR":
-          wires[g[4]] = x ^ y
-          break
-        default:
-          print("Something broke")
-          break
+  var cond = true
+  while cond {
+    cond = false
+    for gate in gates where wires[gate[4]] == nil {
+      cond = true
+      if let x = wires[gate[0]], let y = wires[gate[2]] {
+        switch gate[1] {
+          case "AND": wires[gate[4]] = x && y
+          case "OR": wires[gate[4]] = x || y
+          case "XOR": wires[gate[4]] = x != y
+          default: break
+        }
       }
-
-      gates.remove(at: i)
     }
   }
+
+  let n = wires
+    .filter { $0.key.first! == "z" }
+    .sorted { $0.key > $1.key }
+    .map { $0.value ? "1" : "0" }
+    .joined()
+
+  return Int(n, radix: 2)!
 }
 
-var final = [Character](repeating: "0", count: 50)
-for (key, val) in wires where key[key.startIndex] == "z" {
-  final[Int(key[key.index(after: key.startIndex)..<key.endIndex])!] = Character(String(val))
-}
+let (wires, gates) = getInput()
 
-let total1 = Int(String(final.reversed()), radix: 2)!)
-print("Part 1 answer: \(total1)")
+let ans1 = part1(wires, gates)
+print("Part 1 answer: \(ans1)")
