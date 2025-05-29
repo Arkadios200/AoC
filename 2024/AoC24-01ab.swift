77c45029360b391cf619fedf3f3854c333498fe7
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-05-29 04:40:44 -0700

    Update AoC24-01ab.swift

--- 2024/AoC24-01ab.swift
+++ 2024/AoC24-01ab.swift
@@ -1,3 +1,12 @@
+extension Sequence where Element: Hashable {
+  var contents: [Element: Int] {
+    var contents: [Element: Int] = [:]
+    self.forEach { contents[$0] = (contents[$0] ?? 0) + 1 }
+
+    return contents
+  }
+}
+
 func getInput() -> ([Int], [Int]) {
   var lists: ([Int], [Int]) = ([], [])
   while let line = readLine() {
@@ -15,8 +24,9 @@ let lists = getInput()
 let ans1 = zip(lists.0.sorted(), lists.1.sorted()).reduce(0) { $0 + abs($1.0 - $1.1) }
 print("Part 1 answer: \(ans1)")
 
+let counts = lists.1.contents
 let ans2 = lists.0.reduce(0) {
   (acc, n) in
-  acc + n * lists.1.filter { $0 == n }.count
+  acc + n * (counts[n] ?? 0)
 }
 print("Part 2 answer: \(ans2)")
