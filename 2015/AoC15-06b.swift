43e25ae929a95d2e53f05fc4d3ea9a4a1067543c
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-06-09 17:53:57 -0500

    Update AoC15-06b.swift

--- 2015/AoC15-06b.swift
+++ 2015/AoC15-06b.swift
@@ -1,33 +1,70 @@
-var test = [[Int]](repeating: [Int](repeating: 0, count: 1000), count: 1000)
+func getInput() -> [(String, ClosedRange<Int>, ClosedRange<Int>)] {
+  var lines: [(String, ClosedRange<Int>, ClosedRange<Int>)] = []
+  while let line = readLine() {
+    let temp = line.split(separator: " ")
+    .suffix(4)
+    .joined(separator: " ")
+    .split(separator: " ", maxSplits: 1)
 
-while let line = readLine() {
-  let temp = line.split(separator: " ")
+    let dir = String(temp.first!)
 
-  if temp[0] == "toggle" {
-    let rangeStart = temp[1].split(separator: ",").map( { Int(String($0))! } )
-    let rangeEnd = temp[3].split(separator: ",").map( { Int(String($0))! } )
-    for i in rangeStart[0]...rangeEnd[0] {
-      for j in rangeStart[1]...rangeEnd[1] {
-        test[j][i] += 2
-      }
-    }
-  } else {
-    let rangeStart = temp[2].split(separator: ",").map( { Int(String($0))! } )
-    let rangeEnd = temp[4].split(separator: ",").map( { Int(String($0))! } )
+    let nums = temp.last!.split { !$0.isNumber }.map { Int($0)! }
+    let xRange = nums[0]...nums[2]
+    let yRange = nums[1]...nums[3]
 
-    for i in rangeStart[0]...rangeEnd[0] {
-      for j in rangeStart[1]...rangeEnd[1] {
-        test[j][i] += temp[1] == "off" ? (test[j][i] == 0 ? 0 : -1) : 1
+    lines.append((dir, xRange, yRange))
+  }
+
+  return lines
+}
+
+func part1(_ lines: [(String, ClosedRange<Int>, ClosedRange<Int>)]) -> Int {
+  var grid = [[Bool]](repeating: [Bool](repeating: false, count: 1000), count: 1000)
+
+  for (dir, xRange, yRange) in lines {
+    for i in yRange {
+      for j in xRange {
+        grid[i][j] = {
+          () -> Bool in
+          switch dir {
+            case "on": return true
+            case "off": return false
+            case "toggle": return !grid[i][j]
+            default: fatalError("Something broke")
+          }
+        }()
       }
     }
   }
+
+  return grid.joined().filter { $0 }.count
 }
 
-var total = 0
-for i in test {
-  for j in i {
-    total += j
+func part2(_ lines: [(String, ClosedRange<Int>, ClosedRange<Int>)]) -> Int {
+  var grid = [[Int]](repeating: [Int](repeating: 0, count: 1000), count: 1000)
+
+  for (dir, xRange, yRange) in lines {
+    for i in yRange {
+      for j in xRange {
+        grid[i][j] += {
+          switch dir {
+            case "on": return 1
+            case "off": return grid[i][j] > 0 ? -1 : 0
+            case "toggle": return 2
+            default: fatalError("Something broke")
+          }
+        }()
+      }
+    }
   }
+
+  return grid.joined().reduce(0, +)
 }
 
-print(total)
+let lines = getInput()
+
+let ans1 = part1(lines)
+print("Part 1 answer: \(ans1)")
+
+let ans2 = part2(lines)
+print("Part 2 answer: \(ans2)")
