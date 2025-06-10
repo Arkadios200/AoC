e93652a03efa46c6fbc491f10ecd0e23390da9d9
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-06-10 14:49:18 -0500

    Update AoC15-17ab.swift

--- 2015/AoC15-17ab.swift
+++ 2015/AoC15-17ab.swift
@@ -1,35 +1,45 @@
-let target = 150
-
-var buckets = [Int]()
-while let line = readLine() {
-  buckets.append(Int(line)!)
-}
-
-var total1 = 0
-var counts = [Int]()
-let bin = (1 << buckets.count)
-outer: for i in 0..<bin {
-  var temp = i
-
-  var count = 0
-  var sum = 0
-  for bucket in buckets {
-    if sum > 150 { continue outer }
-    let t = temp % 2
-
-    sum += bucket * t
-    count += t
-    temp /= 2
+func getInput() -> [Int] {
+  var buckets: [Int] = []
+  while let line = readLine() {
+    buckets.append(Int(line)!)
   }
 
-  if sum == target {
-    total1 += 1
-    counts.append(count)
-  }
+  return buckets
 }
 
-print("Part 1 answer: \(total1)")
+func calc(_ buckets: [Int], target: Int) -> [Int] {
+  let bin = 1 << buckets.count
+
+  var counts: [Int] = []
+  outer: for i in 0..<bin {
+    var n = i
+
+    var count = 0
+    var sum = 0
+    for bucket in buckets {
+      if sum > 150 { continue outer }
+      let t = n % 2
+
+      sum += bucket * t
+      count += t
+      n /= 2
+    }
+
+    if sum == target {
+      counts.append(count)
+    }
+  }
+
+  return counts
+}
+
+let buckets = getInput()
+
+let counts = calc(buckets, target: 150)
+
+let ans1 = counts.count
+print("Part 1 answer: \(ans1)")
 
 let min = counts.min()!
-let total2 = counts.filter { $0 == min }.count
-print("Part 2 answer: \(total2)")
+let ans2 = counts.filter { $0 == min }.count
+print("Part 2 answer: \(ans2)")
