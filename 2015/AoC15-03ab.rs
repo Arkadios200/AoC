5011a08d1e1c96d306fb08bc60cf4325dbf32cfb
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-06-15 20:56:11 -0500

    Update AoC15-03ab.rs

--- 2015/AoC15-03ab.rs
+++ 2015/AoC15-03ab.rs
@@ -1,55 +1,60 @@
 use std::fs;
 use std::collections::HashSet;
 
-fn step(pos: &mut (i32, i32), c: char) -> (i32, i32) {
-  match c {
-    '^' => pos.1 -= 1,
-    '>' => pos.0 += 1,
-    'v' => pos.1 += 1,
-    '<' => pos.0 -= 1,
-    _   => { println!("Invalid input."); }
-  };
-
-  *pos
-}
-
-fn part1(input: &str) -> usize {
-  let mut pos: (i32, i32) = (0, 0);
-  let mut pos_record: HashSet<(i32, i32)> = HashSet::new();
-  pos_record.insert(pos);
-  
-  for c in input.chars() {
-    pos_record.insert(step(&mut pos, c));
-  }
-
-  pos_record.len()
-}
-
-
-fn part2(input: &str) -> usize {
-  let mut santa_pos: (i32, i32) = (0, 0);
-  let mut robosanta_pos: (i32, i32) = (0, 0);
-  let mut pos_record: HashSet<(i32, i32)> = HashSet::new();
-  pos_record.insert(santa_pos);
-
-  for (i, c) in input.chars().enumerate() {
-    match i % 2 {
-      0 => {
-        pos_record.insert(step(&mut santa_pos, c));
-      },
-      1 => {
-        pos_record.insert(step(&mut robosanta_pos, c));
-      },
-      _ => { println!("Something broke."); }
-    };
-  }
-
-  pos_record.len()
-}
-
 fn main() {
   let input = fs::read_to_string("input.txt").unwrap();
 
-  println!("Part 1 answer: {}", part1(&input));
-  println!("Part 2 answer: {}", part2(&input));
+  let ans1 = part1(&input);
+  println!("Part 1 answer: {ans1}");
+
+  let ans2 = part2(&input);
+  println!("Part 2 answer: {ans2}");
+}
+
+fn part1(input: &str) -> usize {
+  let mut record: HashSet<Point> = HashSet::new();
+  let mut pos = Point::new();
+  record.insert(pos);
+
+  for c in input.chars() {
+    record.insert(pos.step(c));
+  }
+
+  record.len()
+}
+
+fn part2(input: &str) -> usize {
+  let mut record: HashSet<Point> = HashSet::new();
+  let mut pos: [Point; 2] = [Point::new(), Point::new()];
+  record.insert(pos[0]);
+
+  for (i, c) in input.chars().enumerate() {
+    record.insert(pos[i % 2].step(c));
+  }
+
+  record.len()
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+struct Point {
+  x: i32,
+  y: i32
+}
+
+impl Point {
+  fn new() -> Self {
+    Point { x: 0, y: 0 }
+  }
+
+  fn step(&mut self, c: char) -> Self {
+    match c {
+      '^' => self.y += 1,
+      'v' => self.y -= 1,
+      '>' => self.x += 1,
+      '<' => self.x -= 1,
+      _ => panic!("Invalid direction: {c}")
+    }
+
+    *self
+  }
 }
