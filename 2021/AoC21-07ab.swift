8302fc57de9fdea4f85b561ed3c31224eb7cb092
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-06-21 06:20:07 -0500

    Create AoC21-07ab.swift

---
+++ 2021/AoC21-07ab.swift
@@ -0,0 +1,16 @@
+let input = readLine()!.split(separator: ",").map { Int($0)! }
+
+let ans1 = (input.min()!...input.max()!)
+  .map {
+    n in input.reduce(0) { $0 + abs($1 - n) }
+  }.min()!
+print("Part 1 answer: \(ans1)")
+
+let ans2 = (input.min()!...input.max()!)
+  .map {
+    n in input.reduce(0) {
+      let d = abs($1 - n)
+      return $0 + d * (d + 1) / 2
+    }
+  }.min()!
+print(ans2)
