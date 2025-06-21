54dc91eb59355e06f9307f2767a2ca5946c13cb1
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-06-21 06:29:53 -0500

    Update AoC21-07ab.swift

--- 2021/AoC21-07ab.swift
+++ 2021/AoC21-07ab.swift
@@ -1,16 +1,16 @@
 let input = readLine()!.split(separator: ",").map { Int($0)! }
 
-let ans1 = (input.min()!...input.max()!)
-  .map {
-    n in input.reduce(0) { $0 + abs($1 - n) }
-  }.min()!
+let range = input.min()!...input.max()!
+
+let ans1 = range.map {
+  n in input.reduce(0) { $0 + abs($1 - n) }
+}.min()!
 print("Part 1 answer: \(ans1)")
 
-let ans2 = (input.min()!...input.max()!)
-  .map {
-    n in input.reduce(0) {
-      let d = abs($1 - n)
-      return $0 + d * (d + 1) / 2
-    }
-  }.min()!
+let ans2 = range.map {
+  n in input.reduce(0) {
+    let d = abs($1 - n)
+    return $0 + d * (d + 1) / 2
+  }
+}.min()!
 print("Part 2 answer: \(ans2)")
