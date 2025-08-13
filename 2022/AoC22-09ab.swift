7fc8560bed1218fe2cedfb5d4eb81103e65ed17a
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-08-13 01:57:32 -0400

    Update AoC22-09ab.swift

--- 2022/AoC22-09ab.swift
+++ 2022/AoC22-09ab.swift
@@ -39,38 +39,17 @@ struct Point: Equatable, Hashable {
 
 func getInput() -> [(Character, Int)] {
   var dirs: [(Character, Int)] = []
-  while let line = readLine() {    
+  while let line = readLine(), !line.isEmpty {    
     dirs.append((line.first!, Int(line.split(separator: " ").last!)!))
   }
 
   return dirs
 }
 
-func part1(_ dirs: [(Character, Int)]) -> Int {
+func calc(_ dirs: [(Character, Int)], knots: Int) -> Int {
   var record: Set<Point> = []
 
-  var h = Point(x: 0, y: 0)
-  var t = Point(x: 0, y: 0)
-
-  for (dir, dist) in dirs {
-    for _ in 1...dist {
-      h.step(dir)
-
-      if abs(t.x - h.x) > 1 || abs(t.y - h.y) > 1 {
-        t = Point.steps.map { $0 + t }.min { $0.dist(from: h) < $1.dist(from: h) }!
-      }
-
-      record.insert(t)
-    }
-  }
-
-  return record.count
-}
-
-func part2(_ dirs: [(Character, Int)]) -> Int {
-  var record: Set<Point> = []
-
-  var knots: [Point] = [Point](repeating: Point(x: 0, y: 0), count: 10)
+  var knots: [Point] = [Point](repeating: Point(x: 0, y: 0), count: knots)
 
   for (dir, dist) in dirs {
     for _ in 1...dist {
@@ -82,7 +61,7 @@ func part2(_ dirs: [(Character, Int)]) -> Int {
         }
       }
 
-      record.insert(knots[9])
+      record.insert(knots.last!)
     }
   }
 
@@ -91,8 +70,8 @@ func part2(_ dirs: [(Character, Int)]) -> Int {
 
 let dirs = getInput()
 
-let ans1 = part1(dirs)
+let ans1 = calc(dirs, knots: 2)
 print("Part 1 answer: \(ans1)")
 
-let ans2 = part2(dirs)
+let ans2 = calc(dirs, knots: 10)
 print("Part 2 answer: \(ans2)")
