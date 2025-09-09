421f98f697f90a10692b80d7af0cefa09e483e17
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-09-08 21:16:01 -0500

    Update AoC17-06ab.rs

--- 2017/AoC17-06ab.rs
+++ 2017/AoC17-06ab.rs
@@ -1,34 +1,27 @@
 use std::fs;
 use std::collections::HashSet;
 
-fn redistribute(banks: &mut Vec<u32>) -> usize {
+fn main() {
+  let input = fs::read_to_string("input.txt").unwrap();
+
+  let mut banks: Vec<u32> = input.split_whitespace().map(|s| s.parse().unwrap()).collect();
+
+  println!("Part 1 answer: {}", redistribute(&mut banks));
+  println!("Part 2 answer: {}", redistribute(&mut banks));
+}
+
+fn redistribute(banks: &mut [u32]) -> usize {
   let mut record: HashSet<Vec<u32>> = HashSet::new();
 
   let len = banks.len();
   while record.insert(banks.to_owned()) {
-    let (i, e) = banks.to_owned().into_iter().enumerate()
-    .max_by(|(i, a), (j, b)| {
-      if a == b {
-        j.cmp(&i)
-      } else { 
-        a.cmp(&b)
-      }
-    }).unwrap();
+    let (i, e) = banks.to_owned().into_iter().enumerate().rev().max_by_key(|&(_, n)| n).unwrap();
 
     banks[i] = 0;
-
-    for x in 1..=e as usize {
-      banks[(i + x) % len] += 1;
+    for n in 1..=e as usize {
+      banks[(i + n) % len] += 1;
     }
   }
 
   record.len()
-}
-
-fn main() {
-  let input: String = fs::read_to_string("input.txt").unwrap();
-  let mut banks: Vec<u32> = input.split_whitespace().map(|n| n.parse().unwrap()).collect();
-
-  println!("Part 1 answer: {}", redistribute(&mut banks));
-  println!("Part 2 answer: {}", redistribute(&mut banks));
 }
