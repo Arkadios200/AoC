78c6141406553489213170bb9f5a528f40dce062
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-10-10 19:15:14 -0400

    Update AoC20-03ab.swift

--- 2020/AoC20-03ab.swift
+++ 2020/AoC20-03ab.swift
@@ -1,26 +1,43 @@
-var map = [[Character]]()
-while let line = readLine() {
-  map.append(Array(line))
-}
-
-let width = map[0].count
-let slopes: [(Int, Int)] = [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]
-
-var total = 1
-for slope in slopes {
-  var pos = (0, 0)
-  var temp = 0
-  while pos.0 < map.count {
-    if map[pos.0][pos.1] == "#" {
-      temp += 1
-    }
-    pos = (pos.0 + slope.0, (pos.1 + slope.1) % width)
-  }
-  total *= temp
-
-  if slope == (1, 3) {
-    print("Part 1 answer: \(temp)")
+extension Collection {
+  func get(at index: Index) -> Element? {
+    return indices.contains(index) ? self[index] : nil
   }
 }
 
-print("Part 2 answer: \(total)")
+func getInput() -> [[Character]] {
+  var grid: [[Character]] = []
+  while let line = readLine() {
+    grid.append(Array(line))
+  }
+
+  return grid
+}
+
+func calc(slope: (x: Int, y: Int), _ grid: [[Character]]) -> Int {
+  var pos = (x: 0, y: 0)
+  var out = 0
+
+  while let v = grid.get(at: pos.y) {
+    if v[pos.x] == "#" { out += 1 }
+
+    pos = ((pos.x + slope.x) % v.count, pos.y + slope.y)
+  }
+
+  return out
+}
+
+let grid = getInput()
+
+let ans1 = calc(slope: (3, 1), grid)
+
+let slopes = [
+  (1, 1),
+  (3, 1),
+  (5, 1),
+  (7, 1),
+  (1, 2),
+]
+let ans2 = slopes.reduce(1) { $0 * calc(slope: $1, grid) }
+
+print("Part 1 answer:", ans1)
+print("Part 2 answer:", ans2)
