b99d04059b5414b4a786d4588591116e30a7f7e4
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-12-05 21:02:08 -0600

    Create AoC25-05ab.swift

---
+++ 2025/AoC25-05ab.swift
@@ -0,0 +1,39 @@
+func getInput() -> ([ClosedRange<Int>], [Int]) {
+  var ranges: [ClosedRange<Int>] = []
+  while let line = readLine(), !line.isEmpty {
+    let temp = line.split(separator: "-", maxSplits: 1).map { Int($0)! }
+
+    ranges.append(temp[0]...temp[1])
+  }
+
+  var ids: [Int] = []
+  while let line = readLine() {
+    ids.append(Int(line)!)
+  }
+
+  return (ranges, ids)
+}
+
+func dedup(_ ranges: [ClosedRange<Int>]) -> [ClosedRange<Int>] {
+  let ranges = ranges.sorted { $0.lowerBound < $1.lowerBound }
+
+  var out: [ClosedRange<Int>] = []
+  for r in ranges {
+    if let i = out.firstIndex(where: { $0.overlaps(r) } ) {
+      let a = min(r.lowerBound, out[i].lowerBound)
+      let b = max(r.upperBound, out[i].upperBound)
+
+      out[i] = a...b
+    } else { out.append(r) }
+  }
+
+  return out
+}
+
+let (ranges, ids) = getInput()
+
+let ans1 = ids.filter { id in ranges.contains { $0.contains(id) } }.count
+let ans2 = dedup(ranges).reduce(0) { $0 + $1.count }
+
+print("Part 1 answer:", ans1)
+print("Part 2 answer:", ans2)
