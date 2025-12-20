adc5a02235021231270434885af6e61f08bbc22f
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-12-20 09:38:20 -0600

    Update AoC17-10ab.rs

--- 2017/AoC17-10ab.rs
+++ 2017/AoC17-10ab.rs
@@ -1,5 +1,6 @@
 use std::fs;
-use core::array::from_fn;
+use core::array;
+use std::ops::BitXor;
 
 fn main() {
   let input = fs::read_to_string("input.txt").unwrap();
@@ -12,12 +13,12 @@ fn part1(input: &str) -> u32 {
   let dirs = input.split(',').map(|chunk| chunk.parse::<usize>().unwrap());
 
   const LEN: usize = 256;
-  let mut nums: [u32; LEN] = from_fn(|i| i as u32);
+  let mut nums: [u32; LEN] = array::from_fn(|i| i as u32);
 
   let mut skip: usize = 0;
   let mut i: usize = 0;
   for n in dirs {
-    let a = (i..i+n).map(|x| x % LEN);
+    let a = (i..i+n).map(|x| x & LEN-1);
     for (w, x) in a.to_owned().rev().take(n/2).zip(a) {
       nums.swap(w, x);
     }
@@ -36,13 +37,13 @@ fn knot_hash(input: &str) -> String {
     .collect();
 
   const LEN: usize = 256;
-  let mut nums: [u32; LEN] = from_fn(|i| i as u32);
+  let mut nums: [u32; LEN] = array::from_fn(|i| i as u32);
 
   let mut skip: usize = 0;
   let mut i: usize = 0;
   for _ in 1..=64 {
     for &n in &dirs {
-      let a = (i..i+n).map(|x| x % LEN);
+      let a = (i..i+n).map(|x| x & LEN-1);
       for (w, x) in a.to_owned().rev().take(n/2).zip(a) {
         nums.swap(w, x);
       }
@@ -53,7 +54,7 @@ fn knot_hash(input: &str) -> String {
   }
 
   nums.chunks(16).fold(String::new(), |acc, chunk| {
-    let n = chunk.into_iter().fold(0, |v, num| v ^ num);
+    let n = chunk.into_iter().fold(0, u32::bitxor);
     acc + format!("{n:0>2x}").as_str()
   })
 }
