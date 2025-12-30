a749c8662ac09caaf8354f35c84f6d7ae22c0a75
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-12-30 00:06:46 -0500

    Create AoC25-08ab.swift

---
+++ 2025/AoC25-08ab.swift
@@ -0,0 +1,99 @@
+extension Collection {
+  func unorderedPairs() -> [(Element, Element)] {
+    return Array(self.indices.dropLast().map { i in self[index(after: i)...].map { (self[i], $0) } }.joined())
+  }
+
+  func minBy<K: Comparable>(key: (Element) -> K) -> Element? {
+    return self.min { key($0) < key($1) }
+  }
+
+  func sortedBy<K: Comparable>(key: (Element) -> K) -> [Element] {
+    return self.sorted { key($0) < key($1) }
+  }
+}
+
+struct Point: Equatable, Hashable {
+  let x, y, z: Int
+
+  static let origin: Point = Point(x: 0, y: 0, z: 0)
+
+  func dist(from other: Point = origin) -> Int {
+    let xDist = abs(self.x - other.x)
+    let yDist = abs(self.y - other.y)
+    let zDist = abs(self.z - other.z)
+
+    return xDist * xDist + yDist * yDist + zDist * zDist
+  }
+}
+
+func part1(_ boxes: [Point]) -> Int {
+  var circuits: [Set<Point>] = []
+
+  for (a, b) in boxes.unorderedPairs().sortedBy(key: { $0.0.dist(from: $0.1) } ).prefix(1000) {
+    let i = circuits.firstIndex { $0.contains(a) }
+    let j = circuits.firstIndex { $0.contains(b) }
+
+    if let i = i, let j = j {
+      if i == j { continue }
+
+      for p in circuits[j] { circuits[i].insert(p) }
+      circuits.remove(at: j)
+    } else if let i = i {
+      circuits[i].insert(b)
+    } else if let j = j {
+      circuits[j].insert(a)
+    } else {
+      circuits.append([a, b])
+    }
+  }
+
+  return circuits.map { $0.count }.sorted().suffix(3).reduce(1, *)
+}
+
+func part2(_ boxes: [Point]) -> Int? {
+  var circuits: [Set<Point>] = []
+  var count = 0
+
+  for (a, b) in boxes.unorderedPairs().sortedBy(key: { $0.0.dist(from: $0.1) } ) {
+    let i = circuits.firstIndex { $0.contains(a) }
+    let j = circuits.firstIndex { $0.contains(b) }
+
+    if let i = i, let j = j {
+      if i == j { continue }
+
+      for p in circuits[j] { circuits[i].insert(p) }
+      circuits.remove(at: j)
+    } else if let i = i {
+      circuits[i].insert(b)
+      count += 1
+    } else if let j = j {
+      circuits[j].insert(a)
+      count += 1
+    } else {
+      circuits.append([a, b])
+      count += 2
+    }
+
+    if count == boxes.count {
+      return a.x * b.x
+    }
+  }
+
+  return nil
+}
+
+func getInput() -> [Point] {
+  var points: [Point] = []
+  while let line = readLine(), !line.isEmpty {
+    let temp = line.split(separator: ",", maxSplits: 2).map { Int($0)! }
+
+    points.append(Point(x: temp[0], y: temp[1], z: temp[2]))
+  }
+
+  return points
+}
+
+let boxes = getInput()
+
+print("Part 1 answer:", part1(boxes))
+print("Part 2 answer:", part2(boxes)!)
