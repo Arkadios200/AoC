bbd373f644480ad77d8f082bad11bcebc2b2d140
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2026-01-17 21:53:22 -0800

    Update AoC15-03ab.rs

--- 2015/AoC15-03ab.rs
+++ 2015/AoC15-03ab.rs
@@ -3,33 +3,34 @@ use std::collections::HashSet;
 
 fn main() {
   let input = fs::read_to_string("input.txt").unwrap();
+  let dirs: Vec<Direction> = input
+    .chars()
+    .map(char::into)
+    .collect();
 
-  let ans1 = part1(&input);
-  println!("Part 1 answer: {ans1}");
-
-  let ans2 = part2(&input);
-  println!("Part 2 answer: {ans2}");
+  println!("Part 1 answer: {}", part1(&dirs));
+  println!("Part 2 answer: {}", calc(&dirs, 2));
 }
 
-fn part1(input: &str) -> usize {
+fn part1(dirs: &[Direction]) -> usize {
+  let mut pos = Point::origin();
   let mut record: HashSet<Point> = HashSet::new();
-  let mut pos = Point::new();
   record.insert(pos);
 
-  for c in input.chars() {
-    record.insert(pos.step(c));
+  for dir in dirs {
+    record.insert(pos.step(dir));
   }
 
   record.len()
 }
 
-fn part2(input: &str) -> usize {
+fn calc(dirs: &[Direction], n: usize) -> usize {
+  let mut pos: Vec<Point> = vec![Point::origin(); n];
   let mut record: HashSet<Point> = HashSet::new();
-  let mut pos: [Point; 2] = [Point::new(), Point::new()];
   record.insert(pos[0]);
 
-  for (i, c) in input.chars().enumerate() {
-    record.insert(pos[i % 2].step(c));
+  for (i, dir) in dirs.iter().enumerate() {
+    record.insert(pos[i % n].step(dir));
   }
 
   record.len()
@@ -38,23 +39,45 @@ fn part2(input: &str) -> usize {
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
 struct Point {
   x: i32,
-  y: i32
+  y: i32,
 }
 
 impl Point {
-  fn new() -> Self {
-    Point { x: 0, y: 0 }
+  fn origin() -> Self {
+    Self {
+      x: 0,
+      y: 0,
+    }
   }
 
-  fn step(&mut self, c: char) -> Self {
-    match c {
-      '^' => self.y += 1,
-      'v' => self.y -= 1,
-      '>' => self.x += 1,
-      '<' => self.x -= 1,
-      _ => panic!("Invalid direction: {c}")
-    }
+  fn step(&mut self, dir: &Direction) -> Self {
+    match dir {
+      Direction::Up    => self.y += 1,
+      Direction::Down  => self.y -= 1,
+      Direction::Right => self.x += 1,
+      Direction::Left  => self.x -= 1,
+    };
 
     *self
+  }
+}
+
+#[derive(Clone, Copy)]
+enum Direction {
+  Up,
+  Down,
+  Right,
+  Left,
+}
+
+impl From<char> for Direction {
+  fn from(value: char) -> Self {
+    match value {
+      '^' => Direction::Up,
+      'v' => Direction::Down,
+      '>' => Direction::Right,
+      '<' => Direction::Left,
+      _ => panic!(),
+    }
   }
 }
