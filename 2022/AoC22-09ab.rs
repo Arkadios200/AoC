38f0d1bf44b2d95ff702aa1d6649a002e2128ecf
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2026-01-27 18:28:52 -0600

    Update AoC22-09ab.rs

--- 2022/AoC22-09ab.rs
+++ 2022/AoC22-09ab.rs
@@ -1,51 +1,48 @@
 use std::fs;
 use std::collections::HashSet;
 use std::ops::Add;
+use itertools::Itertools;
 
 fn main() {
   let input = fs::read_to_string("input.txt").unwrap();
 
-  let dirs = process(&input);
+  let dirs: Vec<(Direction, i32)> = input.lines().map(process).collect();
 
-  println!("Part 1 answer: {}", calc(&dirs, 2));
-  println!("Part 2 answer: {}", calc(&dirs, 10));
+  println!("{}", calc(&dirs,  2));
+  println!("{}", calc(&dirs, 10));
 }
 
-fn calc(dirs: &[(char, i32)], knot_count: usize) -> usize {
+fn calc(dirs: &[(Direction, i32)], knots: usize) -> usize {
   let mut record: HashSet<Point> = HashSet::new();
-
-  let mut knots: Vec<Point> = vec![Point::new(); knot_count];
+  let mut rope: Vec<Point> = vec![Point::origin(); knots];
 
   for &(dir, dist) in dirs {
-    for _ in 1..=dist {
-      knots[0].step(&dir);
+    for _ in 0..dist {
+      rope[0].step(&dir);
 
-      /*
-      for (w, k) in knots.to_owned().windows(2).zip(&mut knots[1..]) {
-        if (w[0].x - w[1].x).abs() > 1 || (w[0].y - w[1].y).abs() > 1 {
-          *k = k.adjs().min_by_key(|p| p.dist(&w[0])).unwrap();
-        }
-      }
-      */
-
-      for i in 1usize..knots.len() {
-        if (knots[i-1].x - knots[i].x).abs() > 1 || (knots[i-1].y - knots[i].y).abs() > 1 {
-            knots[i] = knots[i].adjs().min_by_key(|p| p.dist(&knots[i-1])).unwrap();
+      for (i, (a, b)) in (1..).zip(rope.to_owned().iter().tuple_windows()) {
+        if a.diag_dist(b) > 1 {
+          rope[i] = b.adjs().min_by_key(|p| p.m_dist(a)).unwrap();
         }
       }
 
-      record.insert(knots.iter().last().unwrap().to_owned());
+      record.insert(rope.iter().last().unwrap().to_owned());
     }
   }
 
   record.len()
 }
 
-fn process(input: &str) -> Vec<(char, i32)> {
-  input.lines().map(|line| {
-    let (a, b) = line.split_once(' ').unwrap(); 
-    (a.chars().next().unwrap(), b.parse().unwrap())
-  }).collect()
+fn process(line: &str) -> (Direction, i32) {
+  let (a, b) = line.split_once(' ').unwrap();
+
+  (match a.chars().next().unwrap() {
+    'U' => Direction::Up,
+    'D' => Direction::Down,
+    'R' => Direction::Right,
+    'L' => Direction::Left,
+    _ => panic!(),
+  }, b.parse().unwrap())
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
@@ -55,16 +52,24 @@ struct Point {
 }
 
 impl Point {
-  fn new() -> Self {
-    Point { x: 0, y: 0 }
+  fn origin() -> Self {
+    Self {
+      x: 0,
+      y: 0,
+    }
   }
 
-  fn dist(&self, other: &Self) -> i32 {
+  fn m_dist(&self, other: &Self) -> i32 {
     (self.x - other.x).abs() + (self.y - other.y).abs()
+  }
+
+  fn diag_dist(&self, other: &Self) -> i32 {
+    (self.x - other.x).abs().max((self.y - other.y).abs())
   }
 
   fn adjs(&self) -> impl Iterator<Item = Self> {
     let p = *self;
+
     [
       (0, 1),
       (1, 1),
@@ -77,13 +82,12 @@ impl Point {
     ].into_iter().map(move |(x, y)| p + Point { x, y })
   }
 
-  fn step(&mut self, dir: &char) {
+  fn step(&mut self, dir: &Direction) {
     match dir {
-      'U' => self.y += 1,
-      'D' => self.y -= 1,
-      'R' => self.x += 1,
-      'L' => self.x -= 1,
-      _ => panic!("Invalid direction"),
+      Direction::Up    => self.y += 1,
+      Direction::Down  => self.y -= 1,
+      Direction::Right => self.x += 1,
+      Direction::Left  => self.x -= 1,
     }
   }
 }
@@ -91,9 +95,17 @@ impl Point {
 impl Add for Point {
   type Output = Self;
   fn add(self, other: Self) -> Self {
-    Point {
+    Self {
       x: self.x + other.x,
       y: self.y + other.y,
     }
   }
+}
+
+#[derive(Clone, Copy)]
+enum Direction {
+  Up,
+  Down,
+  Right,
+  Left,
 }
