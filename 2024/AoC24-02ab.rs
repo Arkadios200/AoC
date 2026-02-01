a5682140caf224129e9a63fcd917474274696015
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2026-02-01 12:12:05 -0600

    Update AoC24-02ab.rs

--- 2024/AoC24-02ab.rs
+++ 2024/AoC24-02ab.rs
@@ -5,14 +5,14 @@ fn main() {
 
   let reports: Vec<Vec<i32>> = input.lines().map(|line| line.split(' ').map(|s| s.parse().unwrap()).collect()).collect();
 
-  let not_safe = reports.iter().filter(|&r| !is_safe(r.to_owned()));
+  let not_safe = reports.iter().filter(|r| !is_safe(r));
 
   let ans1 = reports.len() - not_safe.to_owned().count();
   let ans2 = ans1 + not_safe.filter(|&r| {
     (0..r.len()).any(|i| {
       let mut r = r.to_owned();
       r.remove(i);
-      is_safe(r)
+      is_safe(&r)
     })
   }).count();
 
@@ -20,8 +20,8 @@ fn main() {
   println!("Part 2 answer: {ans2}");
 }
 
-fn is_safe(mut v: Vec<i32>) -> bool {
-  if v[0] > v[1] { v.reverse(); }
+fn is_safe(v: &[i32]) -> bool {
+  let range = if v[0] < v[1] { 1..=3 } else { -3..=-1 };
 
-  v.windows(2).map(|w| w[1] - w[0]).all(|n| (1..=3).contains(&n))
+  v.windows(2).map(|w| w[1] - w[0]).all(|n| range.contains(&n))
 }
