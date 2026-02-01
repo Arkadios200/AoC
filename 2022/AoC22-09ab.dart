f55813c1922c58e288397dc3bb4df37f891d3031
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2026-02-01 16:37:24 -0600

    Update AoC22-09ab.dart

--- 2022/AoC22-09ab.dart
+++ 2022/AoC22-09ab.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'dart:core';
+import 'dart:math';
 import 'dart:collection';
 
 void main() {
@@ -20,11 +21,11 @@ int calc(List<(String, int)> lines, int len) {
   for (final (dir, dist) in lines) {
     for (int i = 0; i < dist; i++) {
       rope[0].step(dir);
-      for (int j = 1; j < rope.length; j++) {
-        if ((rope[j].x - rope[j-1].x).abs() > 1 || (rope[j].y - rope[j-1].y).abs() > 1) {
-          Point temp = rope[j];
-          for (final Point p in rope[j].adjs) {
-            if (temp.mDist(rope[j-1]) > p.mDist(rope[j-1])) temp = p;
+      for (final (j, (a, b)) in rope.tupleWindows().enumerate(1)) {
+        if (a.diagDist(b) > 1) {
+          Point temp = b;
+          for (final Point p in b.adjs) {
+            if (temp.mDist(a) > p.mDist(a)) temp = p;
           }
 
           rope[j] = temp;
@@ -53,15 +54,15 @@ class Point {
   static final Point origin = Point(0, 0);
 
   List<Point> get adjs => [
-    Point(-1, -1),
-    Point(-1, 0),
-    Point(-1, 1),
-    Point(0, 1),
-    Point(1, 1),
-    Point(1, 0),
-    Point(1, -1),
-    Point(0, -1),
-  ].map((p) => p + this).toList();
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, 1),
+    (1, 1),
+    (1, 0),
+    (1, -1),
+    (0, -1),
+  ].map((a) => Point(a.$1, a.$2) + this).toList();
 
   void step(String dir) {
     switch (dir) {
@@ -76,6 +77,10 @@ class Point {
     return (this.x - other.x).abs() + (this.y - other.y).abs();
   }
 
+  int diagDist(Point other) {
+    return max((this.x - other.x).abs(), (this.y - other.y).abs());
+  }
+
   @override
   bool operator ==(Object o) {
     return o is Point && this.x == o.x && this.y == o.y;
@@ -88,4 +93,26 @@ class Point {
 
   @override
   String toString() => 'Point($x, $y)';
+}
+
+extension <E> on List<E> {
+  List<(E, T)> zip<T>(List<T> other) {
+    List<(E, T)> out = [];
+    for (int i = 0; i < min(this.length, other.length); i++) {
+      out.add((this[i], other[i]));
+    }
+
+    return out;
+  }
+
+  List<(E, E)> tupleWindows() {
+    return this.zip(this.sublist(1));
+  }
+
+  List<(int, E)> enumerate([int start = 0]) {
+    List<(int, E)> out = [];
+    for (E e in this) out.add((start++, e));
+
+    return out;
+  }
 }
