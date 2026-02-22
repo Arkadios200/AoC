eb1322c26cb8d10ca101ccef9f270545d811a33e
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2026-02-22 08:16:00 -0600

    Update AoC23-15ab.swift

--- 2023/AoC23-15ab.swift
+++ 2023/AoC23-15ab.swift
@@ -1,4 +1,16 @@
-import Foundation
+postfix operator +
+
+extension Int {
+  static postfix func + (n: Int) -> Int {
+    return n + 1
+  }
+}
+
+extension Array {
+  mutating func retain(_ cond: (Element) -> Bool) {
+    self = self.filter(cond)
+  }
+}
 
 struct Lens {
   let label: String
@@ -10,41 +22,42 @@ struct Lens {
   }
 }
 
-func hash(_ s: String) -> Int {
-  return s.reduce(0, { (($0 + Int($1.asciiValue!)) * 17) % 256 } )
+func hashAlg(_ s: String) -> Int {
+  return s.reduce(0) { (($0 + Int($1.asciiValue!)) * 17) % 256 }
 }
 
-let input = (try? String(contentsOf: URL(fileURLWithPath: "input.txt")))!.split(separator: ",").map( { String($0) } )
+func part2(_ lines: [String]) -> Int {
+  var boxes: [[Lens]] = [[Lens]](repeating: [], count: 256)
 
-print("Part 1 answer: \(input.reduce(0, { $0 + hash($1) } ))")
+  for line in lines {
+    if let i = line.firstIndex(of: "=") {
+      let label = String(line[..<i])
+      let focalLength = Int(line[line.index(after: i)...])!
 
-var boxes = [[Lens]](repeating: [], count: 256)
-loop: for line in input {
-  let index = line.firstIndex(where: { "-=".contains($0) } )!
-  
-  let label = String(line.prefix(upTo: index))
-  let box = hash(label)
-  
-  switch line[index] {
-    case "-":
-      boxes[box] = boxes[box].filter( { $0.label != label } )
-    case "=":
-      let focalLength = Int(String(line.last!))!
-      if let i = boxes[box].firstIndex(where: { $0.label == label } ) {
-        boxes[box][i].focalLength = focalLength
-      } else {
-        boxes[box].append(Lens(label, focalLength))
+      let pos = hashAlg(label)
+      switch boxes[pos].firstIndex(where: { $0.label == label } ) {
+        case let .some(j):
+          boxes[pos][j].focalLength = focalLength
+        default:
+          boxes[pos].append(Lens(label, focalLength))
       }
-    default:
-      print("Something broke.")
-      break loop
+    } else {
+      let label = String(line.dropLast())
+      boxes[hashAlg(label)].retain { $0.label != label }
+    }
+  }
+
+  return boxes.enumerated().reduce(0) {
+    let (i, box) = $1
+    return $0 + box.enumerated().reduce(0) {
+      let (j, lens) = $1
+      return $0 + i+ * j+ * lens.focalLength
+    }
   }
 }
 
-var total2 = 0
-for (i, box) in boxes.enumerated() {
-  for (j, lens) in box.enumerated() {
-    total2 += (i + 1) * (j + 1) * lens.focalLength
-  }
-}
-print("Part 2 answer: \(total2)")
+let lines = readLine()!.split(separator: ",").map(String.init)
+
+print("Part 1 answer:", lines.reduce(0) { $0 + hashAlg($1) })
+
+print("Part 2 answer:", part2(lines))
