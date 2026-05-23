e75d843e69fc3228ac447ea8bac6dd722cc2c38a
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2026-05-23 18:40:18 -0400

    Update AoC16-01ab.swift

--- 2016/AoC16-01ab.swift
+++ 2016/AoC16-01ab.swift
@@ -1,77 +1,70 @@
 struct Point: Equatable, Hashable {
-  var x: Int
-  var y: Int
+  var x, y: Int
+
+  func mDist(from other: Point = origin) -> Int {
+    return abs(self.x - other.x) + abs(self.y - other.y)
+  }
+
+  static let origin = Point(x: 0, y: 0)
+}
+
+struct Nav {
+  var pos: Point = Point.origin
   var dir: Int = 0
-  var mDist: Int { return abs(x) + abs(y) }
-
-  init(_ x: Int, _ y: Int) {
-    self.x = x
-    self.y = y
-  }
-
-  static func == (lhs: Point, rhs: Point) -> Bool {
-    return lhs.x == rhs.x && lhs.y == rhs.y
-  }
-
-  func hash(into hasher: inout Hasher) {
-    hasher.combine(x)
-    hasher.combine(y)
-  }
 
   mutating func turn(_ d: Character) {
     switch d {
       case "L": dir = (dir + 3) % 4
       case "R": dir = (dir + 1) % 4
-      default: break
+      default: fatalError()
     }
   }
 
   @discardableResult
   mutating func step(_ dist: Int = 1) -> Point {
-    switch dir {
-      case 0: self.y += dist
-      case 1: self.x += dist
-      case 2: self.y -= dist
-      case 3: self.x -= dist
-      default: break
+    switch self.dir {
+      case 0: self.pos.y += dist
+      case 1: self.pos.x += dist
+      case 2: self.pos.y -= dist
+      case 3: self.pos.x -= dist
+      default: fatalError()
     }
 
-    return self
+    return self.pos
   }
 }
 
-func part1(_ input: [(Character, Int)]) -> Int {
-  var pos = Point(0, 0)
-  for (dir, dist) in input {
-    pos.turn(dir)
-    pos.step(dist)
+func part1(_ dirs: [(Character, Int)]) -> Int {
+  var nav = Nav()
+  for (dir, dist) in dirs {
+    nav.turn(dir)
+    nav.step(dist)
   }
 
-  return pos.mDist
+  return nav.pos.mDist()
 }
 
-func part2(_ input: [(Character, Int)]) -> Int {
-  var pos = Point(0, 0)
-  var record: Set = [pos]
+func part2(_ dirs: [(Character, Int)]) -> Int {
+  var record: Set<Point> = []
 
+  var nav = Nav()
   while true {
-    for (dir, dist) in input {
-      pos.turn(dir)
+    for (dir, dist) in dirs {
+      nav.turn(dir)
       for _ in 0..<dist {
-        if !record.insert(pos.step()).inserted {
-          return pos.mDist
+        if !record.insert(nav.step()).inserted {
+          return nav.pos.mDist()
         }
       }
     }
   }
 }
 
-let input: [(Character, Int)] = readLine()!.split { !($0.isNumber || $0.isLetter) }.map {
-  return ($0.first!, Int($0.dropFirst())!)
-}
+let input = readLine()!
 
-let ans1 = part1(input)
-print("Part 1 answer: \(ans1)")
+let dirs: [(Character, Int)] = input
+  .split { !$0.isNumber && !$0.isLetter }
+  .map { ($0.first!, Int($0.dropFirst())!) }
 
-let ans2 = part2(input)
-print("Part 2 answer: \(ans2)")
+print("Part 1 answer:", part1(dirs))
+print("Part 2 answer:", part2(dirs))
