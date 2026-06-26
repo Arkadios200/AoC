784532b6179042fdd8dbe040f13c03fe0b3d8b9d
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2026-06-25 21:03:02 -0400

    Update AoC24-02ab.swift

--- 2024/AoC24-02ab.swift
+++ 2024/AoC24-02ab.swift
@@ -1,28 +1,37 @@
-import struct Foundation.URL
-
-func check(_ v: [Int]) -> Bool {
-  var v = v
-  if v.first! > v.last! { v.reverse() }
-
-  return v.indices.dropFirst().allSatisfy { (1...3).contains(v[$0] - v[$0-1]) }
+extension Sequence {
+  func tupleWindows() -> [(Element, Element)] {
+    return Array(zip(self, self.dropFirst()))
+  }
 }
 
-let input = try String(contentsOf: URL(fileURLWithPath: "input.txt"))
+struct Report {
+  let values: [Int]
 
-let reports: [[Int]] = input.split(separator: "\n").map { $0.split(separator: " ").map { Int($0)! } }
+  func isSafe(tolerance: Int = 0) -> Bool {
+    let sign = values[0] < values[1] ? 1 : -1
+    var check = 0
+    for (a, b) in values.map( { $0 * sign } ).tupleWindows() {
+      if !(1...3).contains(b - a) {
+        check += 1
+        if check > tolerance { return false }
+      }
+    }
 
-let notSafe = reports.filter { !check($0) }
-
-let ans1 = reports.count - notSafe.count
-let ans2 = ans1 + notSafe.filter {
-  r in
-  r.indices.contains {
-    i in
-    var v = r
-    v.remove(at: i)
-    return check(v)
+    return true
   }
-}.count
+}
 
-print("Part 1 answer:", ans1)
-print("Part 2 answer:", ans2)
+func getInput() -> [Report] {
+  var reports: [Report] = []
+  while let line = readLine() {
+    let nums = line.split(separator: " ").map { Int($0)! }
+    reports.append(Report(values: nums))
+  }
+
+  return reports
+}
+
+let reports = getInput()
+
+print("Part 1 answer:", reports.filter { $0.isSafe() }.count)
+print("Part 2 answer:", reports.filter { $0.isSafe(tolerance: 1) }.count)
